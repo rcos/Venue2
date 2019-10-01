679629cb70e50ba2cecb1b1352b046082e1ead2f
tdurkin18 <durkim@rpi.edu>
2019-10-01 16:46:22 -0400

    starter files and navbar

---
+++ venue-app/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <NavBar></NavBar>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import NavBar from "./components/NavBar.vue"
+export default{
+  components: {
+    'NavBar': NavBar
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ venue-app/src/components/NavBar.vue
@@ -0,0 +1,41 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light">
+    <a class="navbar-brand">
+      <img src="../assets/venuelogo.svg" width="30" height="30" class="d-inline-block align-top" alt="">
+      Venue
+      <span class="badge badge-info">ALPHA</span>
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+        <li class="nav-item" data-toggle="collapse" data-target=".navbar-collapse.show">
+          <router-link to="/Classes" class="nav-link">Classes</router-link>
+        </li>
+        <li class="nav-item" data-toggle="collapse" data-target=".navbar-collapse.show">
+          <router-link to="/Grades" class="nav-link">Grades</router-link>
+        </li>
+        <li class="nav-item" data-toggle="collapse" data-target=".navbar-collapse.show">
+          <router-link to="/Account" class="nav-link">Account</router-link>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "NavBar"
+};
+</script>
+
+<style>
+.navbar{
+  background: rgb(124, 71, 146);
+}
+
+.navbar-brand{
+  
+}
+</style>
