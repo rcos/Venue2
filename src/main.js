49d635c35465e51056345a9733082d154abcbb9d
Zach Love <abc811@gmail.com>
2019-10-08 17:19:27 -0400

    Added Buefy and SCSS support, changed header

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import Vue from "vue";
 import App from "./App.vue";
+import Buefy from 'buefy';
+import 'buefy/dist/buefy.css';
+
+Vue.use(Buefy)
 
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
