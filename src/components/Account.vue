617fbc07d20b20e7d8a9c2b976a7229da1c2de30
tdurkin18 <durkim@rpi.edu>
2019-10-11 14:16:45 -0400

    created demo for dashboard

--- src/components/Account.vue
+++ src/components/Account.vue
@@ -7,7 +7,7 @@
 <script>
 export default {
 }
-</script>
+</script scoped>
 
 <style>
 
--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,7 +1,67 @@
 <template>
-  <h1>
-    Recent Submissions
-  </h1>
+  <div>
+    <div class="recent">
+      Recent Submissions
+    </div>
+
+    <div class ="scrolling-wrapper">
+      <div class="card">
+        <h2>Card</h2>
+      </div>
+      <div class="card">
+        <h2>Card</h2>
+      </div>
+      <div class="card">
+        <h2>Card</h2>
+      </div>
+      <div class="card">
+        <h2>Card</h2>
+      </div>
+      <div class="card">
+        <h2>Card</h2>
+      </div>
+      <div class="card">
+        <h2>Card</h2>
+      </div>
+    </div>
+
+    <div class="recent">
+      Current Events
+    </div>
+    <ul class="nav nav-pills">
+      <li class="nav-item">
+        <a class = "nav-link active">CSCI</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link">MATH</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link">COMM</a>
+      </li>
+    </ul>
+
+    <div class ="scrolling-wrapper">
+      <div class="card">
+        <h2>Card</h2>
+      </div>
+      <div class="card">
+        <h2>Card</h2>
+      </div>
+      <div class="card">
+        <h2>Card</h2>
+      </div>
+      <div class="card">
+        <h2>Card</h2>
+      </div>
+      <div class="card">
+        <h2>Card</h2>
+      </div>
+      <div class="card">
+        <h2>Card</h2>
+      </div>
+    </div>
+  </div>
+
 </template>
 
 <script>
@@ -9,10 +69,35 @@ export default {
 }
 </script>
 
-<style>
-h1{
+<style scoped>
+.recent{
+  text-align: center;
+  font-size: 28px;
   padding-top: 10px;
+  padding-bottom: 10px;
+}
+.scrolling-wrapper {
+  overflow-x: scroll;
+  overflow-y: hidden;
+  white-space: nowrap;
+  padding-left: 50px;
+  padding-right: 50px;
+}
+.card{
+  width: 400px;
+  height: 300px;
+  display: inline-block;
+}
+.classes{
   text-align: center;
 }
+.nav-pills {
+  padding-left: 50px;
+  padding-bottom: 5px;
+}
+.nav-link:hover{
+  background: rgb(230,230,230);
+}
+
 
 </style>
