9a199578b2318619f7f063251b64a23b2df68694
Numfor Tiapo <numsmt2@gmail.com>
2019-11-09 21:57:30 -0500

    Begin Course Model

---
+++ src/components/Course/Course.vue
@@ -0,0 +1,131 @@
+<template>
+  <!-- ADDING USER -->
+  <div>
+    <h1>Create A Course</h1>
+    <form @submit.prevent="addCourse">
+      <div class="row">
+        <div class="col-md-6">
+          <div class="form-group">
+            <label>name:</label>
+            <input type="text" class="form-control" v-model="course.name">
+          </div>
+        </div>
+        </div>
+        <div class="row">
+          <div class="col-md-6">
+            <div class="form-group">
+              <label>dept:</label>
+              <input class="form-control" v-model="course.dept">
+            </div>
+          </div>
+        </div><br />
+        <div class="row">
+          <div class="col-md-6">
+            <div class="form-group">
+              <label>course_number:</label>
+              <input type="number" class="form-control" v-model="course.course_number">
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-6">
+            <div class="form-group">
+              <label>instructor:</label>
+              <input class="form-control" v-model="instructor.first_name" readonly>
+              <input class="form-control" v-model="instructor.last_name" readonly>
+            </div>
+          </div>
+        </div>
+        <div class="form-group">
+          <button class="btn btn-primary">Create</button>
+        </div>
+    </form>
+
+  <!-- Showing Users -->
+  <h1>Users</h1>
+    <table class="table table-hover">
+        <thead>
+        <tr>
+          <th>first_name</th>
+          <th>last_name</th>
+          <th>is_instructor</th>
+        </tr>
+        </thead>
+        <tbody>
+            <tr v-for="user in users" :key="user._id">
+              <td>{{ user.first_name }}</td>
+              <td>{{ user.last_name }}</td>
+              <td>{{ user.is_instructor }}</td>
+              <td><button v-if="user.is_instructor" class="btn btn-secondary" @click.prevent="selectInstructor(user._id)">Select</button></td>
+            </tr>
+        </tbody>
+    </table>
+
+
+  <!-- SHOWING Courses -->
+    <h1>Courses</h1>
+      <table class="table table-hover">
+          <thead>
+          <tr>
+            <th>name</th>
+            <th>dept</th>
+            <th>course_number</th>
+            <th>instructor</th>
+          </tr>
+          </thead>
+          <tbody>
+              <tr v-for="course in courses" :key="course._id">
+                <td>{{ course.name }}</td>
+                <td>{{ course.dept }}</td>
+                <td>{{ course.course_number }}</td>
+                <td>{{ course.instructor.first_name }} {{  course.instructor.last_name }}</td>
+                <td><router-link :to="{name: 'editCourse', params: { id: course._id }}" class="btn btn-primary">Edit</router-link></td>
+              </tr>
+          </tbody>
+      </table>
+  </div>
+</template>
+
+<script>
+  import CourseAPI from '@/services/CourseAPI.js';
+  import UserAPI from '@/services/UserAPI.js';
+
+  export default {
+    data(){
+      return {
+        course:{},
+        courses: [],
+        users: [],
+        instructor: {}
+      }
+    },
+    created() {
+      this.loadCourses();
+      this.loadUsers();
+    },
+    methods: {
+      async loadCourses () {
+        const response = await CourseAPI.getCourses();
+        this.courses = response.data;
+      },
+      async loadUsers () {
+        const response = await UserAPI.getUsers();
+        this.users = response.data;
+      },
+      async addCourse(evt){
+        evt.preventDefault(); // prevents the form's default action from redirecting the page
+        this.course.instructor = this.instructor;
+        const response = await CourseAPI.addCourse(this.course);
+        this.courses.push(response.data);
+        this.course = {}; // clear the input field
+      },
+      async deleteCourse(id){
+        const response = await CourseAPI.deleteCourse(id);
+        this.courses.splice(this.courses.findIndex(i => i._id == id), 1);
+      },
+      selectInstructor(id){
+      	this.instructor = this.users.find(user => user._id == id);
+      }
+    }
+  }
+</script>
---
+++ src/components/User/User.vue
@@ -0,0 +1,82 @@
+<template>
+  <!-- ADDING USER -->
+  <div>
+    <h1>Create A User</h1>
+    <form @submit.prevent="addUser">
+      <div class="row">
+        <div class="col-md-6">
+          <div class="form-group">
+            <label>first_name:</label>
+            <input type="text" class="form-control" v-model="user.first_name">
+          </div>
+        </div>
+        </div>
+        <div class="row">
+          <div class="col-md-6">
+            <div class="form-group">
+              <label>last_name:</label>
+              <input class="form-control" v-model="user.last_name">
+            </div>
+          </div>
+        </div><br />
+        <div class="form-group">
+          <button class="btn btn-primary">Create</button>
+        </div>
+    </form>
+
+  <!-- SHOWING USERS -->
+    <h1>Users</h1>
+      <table class="table table-hover">
+          <thead>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>is_instructor</th>
+            <th>is_ta</th>
+          </tr>
+          </thead>
+          <tbody>
+              <tr v-for="user in users" :key="user._id">
+                <td>{{ user.first_name }}</td>
+                <td>{{ user.last_name }}</td>
+                <td>{{ user.is_instructor }}</td>
+                <td>{{ user.is_ta }}</td>
+                <td><router-link :to="{name: 'editUser', params: { id: user._id }}" class="btn btn-primary">Edit</router-link></td>
+                <td><button class="btn btn-danger" @click.prevent="deleteUser(user._id)">Delete</button></td>
+              </tr>
+          </tbody>
+      </table>
+  </div>
+</template>
+
+<script>
+  import UserAPI from '@/services/UserAPI.js';
+
+  export default {
+    data(){
+      return {
+        user:{},
+        users: []
+      }
+    },
+    created() {
+      this.loadUsers();
+    },
+    methods: {
+      async loadUsers () {
+        const response = await UserAPI.getUsers();
+        this.users = response.data;
+      },
+      async addUser(evt){
+        evt.preventDefault(); // prevents the form's default action from redirecting the page
+        const response = await UserAPI.addUser(this.user);
+        this.users.push(response.data);
+        this.user = {}; // clear the input field
+      },
+      async deleteUser(id){
+        const response = await UserAPI.deleteUser(id);
+        this.users.splice(this.users.findIndex(i => i._id == id), 1);
+      }
+    }
+  }
+</script>
