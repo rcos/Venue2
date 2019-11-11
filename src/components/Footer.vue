64ec1af891591516889e153aa1578bad1775e1c9
tdurkin18 <durkim@rpi.edu>
2019-11-11 14:19:22 -0500

    dashboard and grades views

---
+++ src/components/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+    <footer>
+      <a href="" class="black-btn">Venue</a>
+
+    </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer"
+};
+</script>
+
+<style>
+
+</style>
