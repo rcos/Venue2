5d8b5e6243b88ca8e022ba6cf971ac0959f1dbe8
Numfor Tiapo <numsmt2@gmail.com>
2019-11-27 16:00:06 -0500

    Separate User into instructor and Student

--- server/User/User.model.js
+++ server/User/User.model.js
@@ -1,10 +1,11 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+var mongoose = require('mongoose'),
+    extend = require('mongoose-schema-extend');
+var Schema = mongoose.Schema;
 
 let Course = require('../Course/Course.model');
 let Section = require('../Section/Section.model');
 
-//Define collection and schema for User
+//Abstract User Model
 let User = new Schema({
 	first_name: String,
 	last_name: String,
@@ -14,24 +15,65 @@ let User = new Schema({
 		type: Boolean,
 		default: false
 	},
-	courses: [{
-		course: {
-			type: mongoose.Schema.Types.ObjectId,
-			ref: 'Course'
-		}
-	}],
-	ta_sections: [{
-		section: {
-			type: mongoose.Schema.Types.ObjectId,
-			ref: 'Section'
-		}
-	}],
-	submissions: [{
-		submission: {
-			type: mongoose.Schema.Types.ObjectId,
-			ref: 'Submission'
-		}
-	}]
+},
+{ 
+	collection : 'users', 
+	discriminatorKey : '_type' 
+});
+
+//Instructor - Inherits from User
+let Instructor = User.extend({
+  courses: [{
+    course: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Course'
+    }
+  }]
+});
+
+//Student - Inherits from User
+let Student = User.extend({
+  courses: [{
+    course: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Course'
+    }
+  }],
+  sections: [{
+    section: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Section'
+    }
+  }],
+  ta_sections: [{
+    section: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Section'
+    }
+  }],
+  submissions: [{
+    submission: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Submission'
+    }
+  }]
 });
 
 module.exports = mongoose.model('User', User);
+
+//User Model
+//first_name,
+//last_name,
+//email,
+//password,
+//is_instructor
+
+//Instructor Model (inherits from User)
+//courses (courses that the instructor teaches)
+
+//Student Model (inherits from User)
+//courses (courses the student is taking)
+//sections (sections the student is a student for)
+//ta_sections (sections the student is a TA for)
+//submissions 
+
