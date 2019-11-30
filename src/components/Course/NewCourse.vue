27cbc7fc8c6cb9a4d5f8bf0053198aba4f6773bc
Numfor Tiapo <numsmt2@gmail.com>
2019-11-30 11:53:41 -0500

    Fix new course creation

---
+++ src/components/Course/NewCourse.vue
@@ -0,0 +1,79 @@
+<template>
+  <div>
+    <h2>Create A Course</h2>
+    <form @submit.prevent="addCourse">
+      <div class="row">
+        <div class="col-md-6">
+          <div class="form-group">
+            <label>name:</label>
+            <input type="text" class="form-control" v-model="course.name">
+          </div>
+        </div>
+        </div>
+        <div class="row">
+          <div class="col-md-6">
+            <div class="form-group">
+              <label>dept:</label>
+              <input class="form-control" v-model="course.dept">
+            </div>
+          </div>
+        </div><br />
+        <div class="row">
+          <div class="col-md-6">
+            <div class="form-group">
+              <label>course_number:</label>
+              <input type="number" class="form-control" v-model="course.course_number">
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-6">
+            <div class="form-group">
+              <label>instructor:</label>
+              <input class="form-control" v-model="instructor.first_name" readonly>
+              <input class="form-control" v-model="instructor.last_name" readonly>
+            </div>
+          </div>
+        </div>
+        <div class="form-group">
+          <button class="btn btn-primary">Create</button>
+        </div>
+    </form>
+
+  <!-- Showing Instructors -->
+  <Instructors v-on:select-instructor="selectInstructor" />
+
+  </div>
+</template>
+
+<script>
+  import CourseAPI from '@/services/CourseAPI.js';
+  import Instructors from '../User/Instructors'
+
+  export default {
+    name: 'Course',
+    components: {
+      Instructors
+    },
+    data(){
+      return {
+        course: {},
+        instructor: {}
+      }
+    },
+    created() {
+    },
+    methods: {
+      async addCourse(evt){
+        evt.preventDefault();
+        if(typeof this.instructor.first_name !== 'undefined')
+          this.course.instructor = this.instructor;
+        const response = await CourseAPI.addCourse(this.course);
+        this.$router.push({name: 'courses'});
+      },
+      selectInstructor(instructor){
+        this.instructor = instructor
+      },
+    }
+  }
+</script>
