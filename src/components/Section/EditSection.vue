0cf0d51df52dc4446b7caa62be2712e30da52a82
Numfor Tiapo <numsmt2@gmail.com>
2019-12-01 16:14:29 -0500

    Fix EditSection component

--- src/components/Section/EditSection.vue
+++ src/components/Section/EditSection.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <h2>Edit Section</h2>
     <form @submit.prevent="updateSection">
       <div class="row">
         <div class="col-md-6">
@@ -53,33 +54,7 @@
         </tbody>
     </table>
 
-    <h4>Add new Students</h4>
-    <table class="table table-hover">
-        <thead>
-        <tr>
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>is_instructor</th>
-          <th>is_ta</th>
-        </tr>
-        </thead>
-        <tbody>
-            <tr v-for="student in new_students" :key="student._id">
-              <td>{{ student.first_name }}</td>
-              <td>{{ student.last_name }}</td>
-              <td>{{ student.is_instructor }}</td>
-              <td>{{ student.is_ta }}</td>
-              <td><button class="btn btn-danger" @click.prevent="removeNewStudent(student)">Remove</button></td>
-              <!-- <td v-if="is_section_view"><button class="btn btn-secondary" @click.prevent="$emit('select-student', student)">Select</button></td> -->
-            </tr>
-        </tbody>
-    </table>
-
-    <Courses v-bind:is_course_view="false" v-on:select-course="selectCourse" />
-
-    <Instructors v-on:select-instructor="selectInstructor" />
-
-    <Students v-on:select-student="addNewStudent" v-bind:is_section_view="true" />
+    <Students v-on:select-student="addStudent" v-bind:is_section_view="true" />
 
   </div>
 </template>
@@ -100,8 +75,8 @@
     data(){
       return {
         section: {},
-        instructor: {},
         course: {},
+        instructor: {},
         students: [],
         new_students: []
       }
@@ -129,31 +104,20 @@
       async getCurrentSectionStudents(){
         const response = await SectionAPI.getStudents(this.section._id)
         this.students = response.data
-        console.log("Students (len: " + this.students.length + ") " + this.students )
       },
-      selectCourse(course){
-        this.course = course
-      },
-      selectInstructor(instructor){
-        this.instructor = instructor
-      },
-      addNewStudent(student){
-        if(!this.new_students.includes(student) && !this.students.includes(student))
-          this.new_students.push(student)
-      },
-      removeNewStudent(student){
-        this.new_students.splice(this.new_students.indexOf(student),1)
+      addStudent(student){
+        if(!this.students.includes(student))
+          this.students.push(student)
       },
       removeStudent(student){
         this.students.splice(this.students.indexOf(student),1)
       },
       async updateSection() {
         let section_id = this.$route.params.id
-        this.section.instructor = this.instructor
         this.section.course = this.course
-        this.section.students = this.students.concat(this.new_students)
+        this.section.students = this.students
         const response = await SectionAPI.updateSection(section_id, this.section)
-        this.$router.push({name: 'section'})
+        this.$router.push({name: 'sections'})
       }
     }
   }
