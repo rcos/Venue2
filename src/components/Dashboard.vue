7bac1ea67fdd9cd6c92d99e5f2915700cd2730c0
tdurkin18 <durkim@rpi.edu>
2019-12-01 16:40:11 -0500

    new event card design

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -8,30 +8,14 @@
     <div class="container-fluid py-3">
       <h2 class="text-center">Current Events</h2>
       <div class="card-columns">
-        <div class = "card">
-          <img class="card-img-top img-fluid" src="//placehold.it/350x350/" alt="Card image cap">
-        </div>
-        <div class = "card">
-          <img class="card-img-top img-fluid" src="//placehold.it/350x350/" alt="Card image cap">
-        </div>
-        <div class = "card">
-          <img class="card-img-top img-fluid" src="//placehold.it/350x350/" alt="Card image cap">
-        </div>
-        <div class = "card">
-          <img class="card-img-top img-fluid" src="//placehold.it/350x350/" alt="Card image cap">
-        </div>
-        <div class = "card">
-          <img class="card-img-top img-fluid" src="//placehold.it/350x350/" alt="Card image cap">
-        </div>
-        <div class = "card">
-          <img class="card-img-top img-fluid" src="//placehold.it/350x350/" alt="Card image cap">
-        </div>
-        <div class = "card">
-          <img class="card-img-top img-fluid" src="//placehold.it/350x350/" alt="Card image cap">
-        </div>
-        <div class = "card">
-          <img class="card-img-top img-fluid" src="//placehold.it/350x350/" alt="Card image cap">
-        </div>
+        <EventCard></EventCard>
+        <EventCard></EventCard>
+        <EventCard></EventCard>
+        <EventCard></EventCard>
+        <EventCard></EventCard>
+        <EventCard></EventCard>
+        <EventCard></EventCard>
+        <EventCard></EventCard>
       </div>
     </div>
 
@@ -65,47 +49,25 @@
       </div>
     </div>
 
+
   </div>
 
 </template>
 
+
 <script>
+import EventCard from "../components/EventCard";
 export default {
+  name: 'App',
+  components: {
+    EventCard
+  }
 }
+
 </script>
 
 <style scoped>
 
-/* xs only */
-@media(max-width: 767px) {
-  .card-columns {
-    column-count: 2;
-  }
-}
-/* small and up */
-@media(min-width: 768px) {
-  .card-columns {
-    column-count: 2;
-  }
-}
-/* sm only */
-@media(min-width: 768px) and (max-width: 991px) {
-  .card-columns {
-    column-count: 4;
-  }
-}
-/* md and up */
-@media(min-width: 992px) {
-  .card-columns {
-    column-count: 4;
-  }
-}
-/* md only */
-@media(min-width: 992px) and (max-width: 1199px) {
-  .card-columns {
-    column-count: 4;
-  }
-}
 /* lg and up */
 @media(min-width: 1200px) {
   .card-columns {
---
+++ src/components/EventCard.vue
@@ -0,0 +1,90 @@
+<template>
+
+  <div class="card">
+    <div class="card_image">
+      <img src="../assets/venuelogo.svg" />
+      </div>
+    <div class="card_Event title-white">
+      <p>Event Name</p>
+    </div>
+    <div class="card_Class title-white">
+      <p>Class Name</p>
+    </div>
+    <div class="card_Time title-white">
+      <p>Time</p>
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+};
+</script>
+
+<style scoped>
+.card {
+  margin: 30px auto;
+  width: 250px;
+  height: 250px;
+  border-radius: 40px;
+  cursor: pointer;
+  transition: 0.4s;
+  background-color: DarkGray
+}
+
+.card .card_image {
+  width: inherit;
+  height: inherit;
+  border-radius: 40px;
+}
+
+.card .card_image img {
+  width: inherit;
+  height: inherit;
+  border-radius: 40px;
+  object-fit: cover;
+}
+
+.card .card_Event {
+  text-align: center;
+  border-radius: 0px 0px 40px 40px;
+  font-family: sans-serif;
+  font-weight: bold;
+  font-size: 30px;
+  margin-top: -150px;
+  padding-bottom: 150px;
+  height: 40px;
+}
+
+.card .card_Class {
+  text-align: center;
+  border-radius: 0px 0px 40px 40px;
+  font-family: sans-serif;
+  font-weight: bold;
+  font-size: 20px;
+  margin-top: -80px;
+}
+
+.card:hover {
+  transform: scale(0.9, 0.9);
+  box-shadow: 5px 5px 10px 10px rgba(0,0,0,0.25),
+    -5px -5px 10px 10px rgba(0,0,0,0.22);
+}
+
+.title-white {
+  color: white;
+}
+
+.title-black {
+  color: black;
+}
+
+@media all and (max-width: 500px) {
+  .card-list {
+    /* On small screens, we are no longer using row direction but column */
+    flex-direction: column;
+  }
+}
+
+</style>
