6b809fb7cb28584581c9da5757b47787ae728ddf
Numfor Tiapo <numsmt2@gmail.com>
2020-01-21 16:15:03 -0500

    Add LandingInfo Component

---
+++ src/components/LandingInfo.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container" style="border:red solid;">
+    <img src="../assets/venuelogo.svg" style="width:15rem; padding-top:2rem;" />
+    <div style="font-size:7rem; color:black;">Venue</div>
+    <div style="font-size:1.5rem; font-style:italic;">secure attendance</div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'LandingInfo',
+    data() {
+      return {
+        user: {}
+      }
+    },
+    created() {
+    },
+    methods: {
+    }
+  }
+</script>
