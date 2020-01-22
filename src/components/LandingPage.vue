540ad9f0f2999939ae2c03fbddbe48a9464faba8
Numfor Tiapo <numsmt2@gmail.com>
2020-01-21 22:56:58 -0500

    Add Login Buttons

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -1,26 +1,34 @@
 <template>
   <div>
     <LandingInfo />
-    <LoginSignupButtons />
+    <LoginSignupButtons v-on:show-login-buttons="showLoginButtons" v-if="show_login_signup_buttons" />
+    <LoginButtons v-else />
   </div>
 </template>
 
 <script>
   import LandingInfo from './LandingInfo.vue'
   import LoginSignupButtons from './LoginSignupButtons.vue'
+  import LoginButtons from './LoginButtons.vue'
 
   export default {
     components: {
       LandingInfo,
-      LoginSignupButtons
+      LoginSignupButtons,
+      LoginButtons
     },
     data() {
       return {
+        show_login_signup_buttons: true
       }
     },
     created() {
     },
     methods: {
+      showLoginButtons() {
+        console.log("I enter this function")
+        this.show_login_signup_buttons = !this.show_login_signup_buttons
+      }
     }
   }
 </script>
