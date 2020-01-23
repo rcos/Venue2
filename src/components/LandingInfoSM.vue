ab088f5b0eedfa46028542097a9eb3b43e574f09
Numfor Tiapo <numsmt2@gmail.com>
2020-01-22 23:22:44 -0500

    Begin Smaller LandingInfo Component

---
+++ src/components/LandingInfoSM.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container" style="border: black solid;">
+    <img src="../assets/venuelogo.svg" style="width:18rem; border: green solid; display:inline-block;" />
+    <!-- <div style="font-size:8rem; color:black; display:inline-block; border: blue solid;">Venue</div> -->
+<!--     <div style="font-size:1.5rem; font-style:italic;">
+      <span style="color:#e83e8c">secure </span> 
+      <span style="color:#007bff">attendance</span>
+    </div> -->
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'LandingInfo',
+    data() {
+      return {
+      }
+    },
+    created() {
+    },
+    methods: {
+    }
+  }
+</script>
+
+<style scoped>
+
+<style>
