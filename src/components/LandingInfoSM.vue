604b2d9a401ffc576579e615aab3a74a2ac01972
Numfor Tiapo <numsmt2@gmail.com>
2020-01-23 21:30:38 -0500

    Style LandingInfoSM

--- src/components/LandingInfoSM.vue
+++ src/components/LandingInfoSM.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="container" style="border: black solid;">
-    <img src="../assets/venuelogo.svg" style="width:18rem; border: green solid; display:inline-block;" />
-    <!-- <div style="font-size:8rem; color:black; display:inline-block; border: blue solid;">Venue</div> -->
+  <div class="container" style="margin-top:2rem;">
+    <img src="../assets/venuelogo.svg" style="width:10rem;" />
+    <div style="font-size:4rem; color:black;">Venue</div>
 <!--     <div style="font-size:1.5rem; font-style:italic;">
       <span style="color:#e83e8c">secure </span> 
       <span style="color:#007bff">attendance</span>
