41f33152142455f1d282f02a71f0a9a82c5b2999
Numfor Tiapo <numsmt2@gmail.com>
2020-01-24 10:59:07 -0500

    Remove Signup functionality

--- src/components/LoginButton.vue
+++ src/components/LoginButton.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="container" style="padding:2rem;">
     <button type="button" class="btn button shadow" id="login" 
-      @click="$emit('show-login-buttons')">Login</button>
-    <button type="button" class="btn button shadow" id="signup"
-      @click="$emit('show-signup-buttons')">Sign Up</button>
+      @click="$emit('show-login-form')">Login</button>
+<!--     <button type="button" class="btn button shadow" id="signup"
+      @click="$emit('show-signup-buttons')">Sign Up</button> -->
   </div>
 </template>
 
 <script>
   export default {
-    name: 'LoginSignupButtons',
+    name: 'LoginButton',
     data() {
       return {
       }
@@ -26,8 +26,8 @@
   background-color: white;
   border-radius: 0%;
   font-size: 1.5rem;
-  margin-right: 2rem;
-  margin-left: 2rem;
+/*  margin-right: 2rem;
+  margin-left: 2rem;*/
   width: 8rem;
   margin-top: 1.5rem;
   border: white solid;
