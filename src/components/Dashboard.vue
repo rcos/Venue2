e4c06c504b3af14665d81ff5f12ade4b4af2c41f
Numfor Tiapo <numsmt2@gmail.com>
2020-02-06 13:14:24 -0500

    Add Dashboard component

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h2>Dashboard</h2>
+  </div>
+</template>
+
+<script>
+  import UserAPI from '@/services/UserAPI.js';
+
+  export default {
+    name: 'Dashboard',
+    data(){
+      return {
+        user: {}
+      }
+    },
+    created() {
+    },
+    methods: {
+      async signUp(evt){
+        evt.preventDefault(); // prevents the form's default action from redirecting the page
+        const response = await UserAPI.addUser(this.user);
+        this.$router.push({name: 'users'});
+      },
+    }
+  }
+</script>
