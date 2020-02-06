95af920c49e0cb8fb685b267c7084958fcedc4ca
Numfor Tiapo <numsmt2@gmail.com>
2020-02-06 13:28:08 -0500

    Add vuex

--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -58,11 +58,17 @@
     created() {
     },
     methods: {
-      async signUp(evt){
-        evt.preventDefault(); // prevents the form's default action from redirecting the page
-        const response = await UserAPI.addUser(this.user);
-        this.$router.push({name: 'users'});
-      },
+      // async signUp(evt){
+      //   evt.preventDefault(); // prevents the form's default action from redirecting the page
+      //   const response = await UserAPI.addUser(this.user);
+      //   this.$router.push({name: 'users'});
+      // },
+      signUp() {
+        this.$store
+          .dispatch('signup', {
+            user: this.user
+          })
+      }
     }
   }
 </script>
