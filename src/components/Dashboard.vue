4c2abe0025915f77c599377d4669030ccc87c59f
Numfor Tiapo <numsmt2@gmail.com>
2020-02-07 11:45:13 -0500

    Add loggedIn mixin

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -3,15 +3,21 @@
     <h2>Dashboard</h2>
     <button type="submit" class="btn btn-primary" @click="logOut">Logout</button>
     <Users />
+    <h2 v-if="loggedIn">I'm logged in</h2>
+    <h2 v-else>Not logged in</h2>
   </div>
 </template>
 
 <script>
   import UserAPI from '@/services/UserAPI.js';
   import Users from './User/Users'
+  import { authComputed } from '../vuex/helpers.js'
 
   export default {
     name: 'Dashboard',
+    computed: {
+      ...authComputed
+    },
     components: {
       Users
     },
