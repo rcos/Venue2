a0356f1e230c94ffb244d90e76838de1de4e65a9
Numfor Tiapo <numsmt2@gmail.com>
2020-02-07 11:41:14 -0500

    Prevent User from inputting fake credentials

--- src/main.js
+++ src/main.js
@@ -26,6 +26,16 @@ new Vue({
     		const userData = JSON.parse(userString)
     		this.$store.commit('SET_USER_DATA', userData)
     	}
+    	//Prevents user from inputting fake credentials
+    	axios.interceptors.response.use(
+    		response => response,
+    		error => {
+    			if (error.response.status === 401) {
+    				this.$store.dispatch('logout')
+    			}
+    			return Promise.reject(error)
+    		}
+    	)
     },
     render: h => h(App)
 }).$mount('#app');
