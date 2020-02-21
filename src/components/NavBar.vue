1a8083d0224f708c6d48456c0c32ec8f79fa070b
Numfor Tiapo <numsmt2@gmail.com>
2020-02-21 11:14:57 -0500

    Add nav buttons to navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,10 +1,26 @@
 <template>
   <div>
-    <nav class="navbar navbar-light venue-nav">
+    <nav class="navbar navbar-light venue-nav navbar-expand-lg">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
       <a class="navbar-brand" :href="'/dashboard'">
         <img src="@/assets/venuelogo.svg" width="30" height="30" class="d-inline-block align-top" alt="">
         Venue
       </a>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav mx-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="#">Courses</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Events</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Grading</a>
+          </li>
+        </ul>
+      </div>
       <div class="float-right">
         <p class="d-inline-block mr-2">Numfor Tiapo</p>
         <img src="@/assets/settings.svg" width="20" height="20" class="d-inline-block align-top settings" alt="">
