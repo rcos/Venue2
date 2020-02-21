9c325d0e85c97976b29f46fa53a69e9eb4d2f63d
Numfor Tiapo <numsmt2@gmail.com>
2020-02-21 10:42:17 -0500

    Link Navbar logo to dashboard

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,25 +1,45 @@
 <template>
   <div>
-    <nav class="navbar navbar-light bg-light">
-      <a class="navbar-brand" href="#">
-        <img src="/docs/4.0/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="">
-        Bootstrap
+    <nav class="navbar navbar-light venue-nav">
+      <a class="navbar-brand" :href="'/dashboard'">
+        <img src="@/assets/venuelogo.svg" width="30" height="30" class="d-inline-block align-top" alt="">
+        Venue
       </a>
     </nav>
   </div>
 </template>
 
 <script>
+  import UserAPI from '@/services/UserAPI.js';
+
   export default {
     name: 'NavBar',
-    data() {
-
+    computed: {
+    },
+    components: {
+    },
+    data(){
+      return {
+      }
     },
     created() {
-
+      this.getCurrentUser()
     },
-    methods() {
-
+    methods: {
+      // Get user from localstorage
+      getCurrentUser() {
+        const userString = localStorage.getItem('user')
+        const userData = JSON.parse(userString)
+        console.log("user: " + userData)
+      }
     }
   }
 </script>
+
+<style scoped>
+  .venue-nav {
+    border-style: none none solid none;
+    border-bottom: #e0e0e0 solid thin;
+    padding-left: 1.5rem;
+  }
+</style>
