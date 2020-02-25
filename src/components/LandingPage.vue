47d079b1f8adcef2a9165772d45d89fd176ff392
Abdul <muiz.rpi@gmail.com>
2020-02-25 14:35:59 -0500

    Updated landing page

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -1,16 +1,16 @@
 <template>
-  <div>
-    <LandingInfo v-if="!show_login_form" />
+  <div class="page-overlap">
+    <!-- <LandingInfo v-if="!show_login_form" />
     <LandingInfoSM v-if="show_login_form" />
-    <LoginButton v-if="show_login_button" 
+    <LoginButton v-if="show_login_button"
     v-on:show-login-form="showLoginForm"/>
-    <LoginForm v-if="show_login_form" v-on:show-login-button="showLoginButton" />
+    <LoginForm v-if="show_login_form" v-on:show-login-button="showLoginButton" /> -->
 
     <!-- For sign up form feature -->
 <!--     <LandingInfo v-if="!show_login_form && !show_student_signup_form && !show_instructor_signup_form" />
-    <LoginSignupButtons v-if="show_login_signup_buttons" 
+    <LoginSignupButtons v-if="show_login_signup_buttons"
     v-on:show-login-buttons="showLoginButtons" v-on:show-signup-buttons="showSignupButtons" />
-    <LoginButtons v-if="show_login_buttons" v-on:show-login-form="showLoginForm" 
+    <LoginButtons v-if="show_login_buttons" v-on:show-login-form="showLoginForm"
       v-on:show-signup-buttons="showSignupButtons"/>
     <SignupButtons v-if="show_signup_buttons" v-on:show-student-signup-form="showStudentSignupForm"
       v-on:show-instructor-signup-form="showInstructorSignupForm"
@@ -19,29 +19,39 @@
     <LoginForm v-if="show_login_form" v-on:show-login-buttons="showLoginButtons" />
     <StudentSignupForm v-if="show_student_signup_form" v-on:show-signup-buttons="showSignupButtons" />
     <InstructorSignupForm v-if="show_instructor_signup_form" v-on:show-signup-buttons="showSignupButtons" /> -->
+    <div :style="{maxWidth: '500px', margin: '0 auto'}">
+      <Logo :scale="3" :textScale="2" :minimized="false" :style="{marginBottom: '0px', marginTop: '150px'}"/>
+      <Button value="Get Started" :onClick="()=>{ alert('Clicked!'); }" :style="{marginTop: '150px'}" />
+    </div>
+
   </div>
 </template>
 
 <script>
-  import LandingInfo from './LandingInfo.vue'
-  import LoginButton from './LoginButton.vue'
-  import LoginButtons from './LoginButtons.vue'
-  import SignupButtons from './SignupButtons.vue'
-  import LoginForm from './Forms/LoginForm.vue'
-  import LandingInfoSM from './LandingInfoSM.vue'
-  import StudentSignupForm from './Forms/StudentSignupForm.vue'
-  import InstructorSignupForm from './Forms/InstructorSignupForm.vue'
+  // import LandingInfo from './LandingInfo.vue'
+  // import LoginButton from './LoginButton.vue'
+  // import LoginButtons from './LoginButtons.vue'
+  // import SignupButtons from './SignupButtons.vue'
+  // import LoginForm from './Forms/LoginForm.vue'
+  // import LandingInfoSM from './LandingInfoSM.vue'
+  // import StudentSignupForm from './Forms/StudentSignupForm.vue'
+  // import InstructorSignupForm from './Forms/InstructorSignupForm.vue'
+
+  import Logo from './UI/Logo.vue'
+  import Button from './UI/Button.vue'
 
   export default {
     components: {
-      LandingInfo,
-      LoginButton,
-      LoginButtons,
-      SignupButtons,
-      LoginForm,
-      LandingInfoSM,
-      StudentSignupForm,
-      InstructorSignupForm
+      // LandingInfo,
+      // LoginButton,
+      // LoginButtons,
+      // SignupButtons,
+      // LoginForm,
+      // LandingInfoSM,
+      // StudentSignupForm,
+      // InstructorSignupForm
+      Logo,
+      Button
     },
     data() {
       return {
@@ -66,34 +76,6 @@
         this.show_login_form = true
         this.show_login_button = false
       }
-      // showLoginButtons() {
-      //   this.setVisibilityFlagsToFalse()
-      //   this.show_login_buttons = true
-      // },
-      // showSignupButtons() {
-      //   this.setVisibilityFlagsToFalse()
-      //   this.show_signup_buttons = true
-      // },
-      // showLoginForm() {
-      //   this.setVisibilityFlagsToFalse()
-      //   this.show_login_form = true
-      // },
-      // showStudentSignupForm() {
-      //   this.setVisibilityFlagsToFalse()
-      //   this.show_student_signup_form = true
-      // },
-      // showInstructorSignupForm() {
-      //   this.setVisibilityFlagsToFalse()
-      //   this.show_instructor_signup_form = true
-      // },
-      // setVisibilityFlagsToFalse() {
-      //   this.show_login_signup_buttons = false
-      //   this.show_login_buttons = false
-      //   this.show_signup_buttons = false
-      //   this.show_login_form = false
-      //   this.show_student_signup_form = false
-      //   this.show_instructor_signup_form = false
-      // }
     }
   }
 </script>
