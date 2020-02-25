5bce2f586e5aa602911c322bccf67fd1542490a2
Abdul <muiz.rpi@gmail.com>
2020-02-25 14:35:59 -0500

    Updated landing page

--- src/components/UI/Logo.vue
+++ src/components/UI/Logo.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="logo-container">
-    <img src="../assets/venue-logo.svg" :width="renderSize(scale)"/>
-    <div v-if="!minimized" class="logo-text">venue</div>
-    <div v-if="!minimized" class="sub-logo"><span class="logo-secure">secure</span><span class="logo-attendance">attendance</span></div>
+    <img src="../../assets/venue-logo.svg" :width="renderSize(scale)"/>
+    <div v-if="!minimized" :style="{fontSize: '' + renderLogoFontSize(textScale)}" class="logo-text">venue</div>
+    <div v-if="!minimized" :style="{fontSize: '' + renderSublogoFontSize(textScale)}" class="sub-logo">
+      <span class="logo-secure">secure </span><span class="logo-attendance"> attendance</span>
+    </div>
   </div>
 </template>
 
@@ -17,6 +19,10 @@ export default {
     scale: {
       type: Number,
       default: 1
+    },
+    textScale: {
+      type: Number,
+      default: 1
     }
   },
   methods: {
@@ -26,9 +32,31 @@ export default {
           return "50px"
         case 2:
           return "80px"
+        case 3:
+          return "120px"
         default:
           return "50px"
       }
+    },
+    renderLogoFontSize: (x) => {
+      switch(x) {
+        case 1:
+          return '25px'
+        case 2:
+          return '32px'
+        default:
+          return '25px'
+      }
+    },
+    renderSublogoFontSize: (x) => {
+        switch(x) {
+          case 1:
+            return '12px'
+          case 2:
+            return '15px'
+          default:
+            return '25px'
+        }
     }
   }
 }
