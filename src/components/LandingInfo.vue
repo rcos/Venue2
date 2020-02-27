3c3e0caf9004659b567fafed92c7d01247031adc
Numfor Tiapo <numsmt2@gmail.com>
2020-02-26 23:13:30 -0500

    Change sub-logo text

--- src/components/LandingInfo.vue
+++ src/components/LandingInfo.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <img src="@/assets/venue-logo.svg" style="width:15rem; padding-top:2rem;" />
     <div class="logo-text" style="font-size:8rem;">venue</div>
-    <div style="font-size:1.5rem; font-style:italic;">
+    <div class="sub-logo" style="font-size:1.5rem;">
       <span style="color:#e83e8c">secure </span> 
       <span style="color:#007bff">attendance</span></div>
   </div>
