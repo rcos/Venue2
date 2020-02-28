78af5ef3d6cb7dc49962e365ba72dc06f3710e00
Numfor Tiapo <numsmt2@gmail.com>
2020-02-27 22:17:27 -0500

    Change Navbar scaling to work on medium size screens

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,22 +1,21 @@
 <template>
   <div>
     <nav class="navbar navbar-light venue-nav navbar-expand-lg">
-      <hide-at breakpoint="small">
+      <hide-at breakpoint="mediumAndBelow">
         <a class="navbar-brand navbar-logo" :href="'/dashboard'">
           <img src="@/assets/venue-logo.svg" width="30" height="30" class="d-inline-block align-top" alt="">
         </a>
       </hide-at>
-      <show-at breakpoint="small">
+      <show-at breakpoint="mediumAndBelow">
         <a class="navbar-brand navbar-logo" style="border:none;" data-toggle="collapse" data-target="#navbarNav">
           <img src="@/assets/venue-logo.svg" width="30" height="30" class="d-inline-block align-top" alt="">
         </a>
       </show-at>
-      <div class="float-right">
-        <p class="d-inline-block mr-2">Numfor Tiapo</p>
-        <hide-at breakpoint="small">
-          <img src="@/assets/settings.svg" width="20" height="20" class="d-inline-block align-top settings" alt="">
-        </hide-at>
-      </div>
+      <show-at breakpoint="mediumAndBelow">
+        <div class="float-right">
+          <p class="d-inline-block mr-2">Numfor Tiapo</p>
+        </div>
+      </show-at>
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav mx-auto">
           <li class="nav-item">
@@ -31,7 +30,7 @@
             <a v-if="this.$route.name === 'grading'" class="nav-link active" href="#">Grading</a>
             <a v-else class="nav-link" href="#">Grading</a>
           </li>
-          <show-at breakpoint="small">
+          <show-at breakpoint="mediumAndBelow">
             <li class="nav-item">
               <a v-if="this.$route.name === 'settings'" class="nav-link active" href="#">Settings</a>
               <a v-else class="nav-link" href="#">Settings</a>
@@ -39,6 +38,12 @@
           </show-at>
         </ul>
       </div>
+      <hide-at breakpoint="mediumAndBelow">
+        <div class="float-right">
+          <p class="d-inline-block mr-2">Numfor Tiapo</p>
+          <img src="@/assets/settings.svg" width="20" height="20" class="d-inline-block align-top settings" alt="">
+        </div>
+      </hide-at>
 
     </nav>
 
