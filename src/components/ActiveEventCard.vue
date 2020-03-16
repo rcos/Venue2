92295cd021802e07efc3f34be6e9e5eeb49eda74
Numfor Tiapo <numsmt2@gmail.com>
2020-03-15 22:49:46 -0400

    Show Remaining time in ActiveEventCard

--- src/components/ActiveEventCard.vue
+++ src/components/ActiveEventCard.vue
@@ -10,21 +10,12 @@
     </div>
     <div class="event-card-section" id="time-section">
       <img src="@/assets/clock.svg" class="clock">
-      <div class="time-remaining">35m</div>
+      <div class="time-remaining">
+        <span v-if="remaining_hours > 0">{{ remaining_hours }}h </span>
+        <span v-if="remaining_mins > 0">{{ remaining_mins }}m</span>
+      </div>
     </div>
   </div>
-<!-- <div class="desktop-active-class-modal">
-  <div class="bg"></div>
-  <div class="fg">
-    <div class="class-info">
-      <div class="class-name">Intro to AI</div>
-      <div class="class-code">CSCI 1200</div>
-    </div>
-    <div class="time-remaining">
-      35m
-    </div>
-  </div>
-</div> -->
 </template>
 
 <script>
@@ -42,7 +33,9 @@ export default {
   data(){
     return {
       section: {},
-      course: {}
+      course: {},
+      remaining_hours: Number,
+      remaining_mins: Number
     }
   },
   created() {
@@ -54,12 +47,24 @@ export default {
       this.section = response.data
       this.course = this.section.course
       this.adjustCourseNameForViewing()
+      this.getRemainingTime()
     },
     adjustCourseNameForViewing() {
       if(this.course.name.length > 18){
         this.course.name = this.course.name.slice(0,11)
         this.course.name += "..."
       }
+    },
+    getRemainingTime() {
+      let current_time = new Date()
+      let event_end_time = new Date(this.event.end_time)
+      let diff_milliseconds = Math.abs(event_end_time - current_time);
+      let diff_hours = Math.floor((diff_milliseconds % 86400000) / 3600000); // hours
+      let diff_mins = Math.round(((diff_milliseconds % 86400000) % 3600000) / 60000); // minutes
+      console.log(diff_hours)
+      console.log(diff_mins)
+      this.remaining_hours = diff_hours
+      this.remaining_mins = diff_mins
     }
   }
 }
@@ -160,65 +165,4 @@ export default {
   }
 }
 
-.desktop-active-class-modal {
-  width: 90%;
-  margin: 0 auto;
-  border-radius: 5px;
-  height: 70px;
-  position: relative;
-}
-
-.desktop-active-class-modal .bg,
-.desktop-active-class-modal .fg {
-  position: absolute;
-}
-
-.desktop-active-class-modal .bg {
-  background-color: #FC5D60;
-  width: 104%;
-  height: 70px;
-  left: -2%;
-  border-radius: 5px;
-}
-
-.desktop-active-class-modal .fg {
-  background-color: white;
-  width: 100%;
-  top: 6px;
-  height: 70px;
-  border-radius: 5px;
-  box-sizing: border-box;
-  padding: 5px 20px;
-  box-shadow: 0px 3px 10px 5px rgba(0, 0, 0, 0.1);
-}
-
-.desktop-active-class-modal .time-remaining,
-.desktop-active-class-modal .class-info {
-  display: inline-block;
-  vertical-align: top;
-}
-
-.desktop-active-class-modal .time-remaining {
-  width: 12%;
-  color: #FF7B7B;
-  font-size: 13px;
-  text-align: right;
-  height: 70px;
-  line-height: 70px;
-  position: relative;
-  top: -5px;
-}
-.desktop-active-class-modal .class-info {
-  width: 85%;
-}
-
-.desktop-active-class-modal .class-info .class-name {
-  font-size: 15px;
-  margin-top: 10px;
-}
-
-.desktop-active-class-modal .class-info .class-code {
-  font-size: 12px;
-  color: #1591C5;
-}
 </style>
