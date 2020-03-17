73c5acd5b9601d09158f94135c2e36758db608db
Numfor Tiapo <numsmt2@gmail.com>
2020-03-16 21:26:01 -0400

    Add CourseCard Component

---
+++ src/components/CourseCard.vue
@@ -0,0 +1,36 @@
+<<template>
+  <div>
+ 	I'm a course card
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name: 'CourseCard',
+    props: {
+
+    },
+    computed: {
+    },
+    components: {
+
+    },
+    data(){
+      return {
+
+      }
+    },
+    created() {
+
+    },
+    methods: {
+    }
+
+  }
+</script>
+
+<style scoped>
+
+
+</style>
