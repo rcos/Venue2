6999f3e46b54363fa1f83439477d14fa650a2b30
Numfor Tiapo <numsmt2@gmail.com>
2020-03-17 14:15:00 -0400

    Add Courses Component

---
+++ src/components/Courses.vue
@@ -0,0 +1,32 @@
+<<template>
+  <div>
+    <CourseCard v-for="course in courses" />
+  </div>
+</template>
+
+<script>
+  import CourseCard from '@/components/CourseCard.vue'
+
+  export default {
+    name: 'Courses',
+    props: {
+      courses: Array
+    },
+    components: {
+      CourseCard
+    },
+    data(){
+      return {
+
+      }
+    },
+    created() {
+    },
+    methods: {
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
