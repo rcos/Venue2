d46e76a1842034e4c1f30d66b5d7e7f508586880
Numfor Tiapo <numsmt2@gmail.com>
2020-03-17 14:25:13 -0400

    Show CourseList Component at user_courses route

--- src/components/CourseList.vue
+++ src/components/CourseList.vue
@@ -6,27 +6,33 @@
 
 <script>
   import CourseCard from '@/components/CourseCard.vue'
+  import CourseAPI from '@/services/CourseAPI.js'
 
   export default {
     name: 'CourseList',
-    props: {
-      courses: Array
-    },
     components: {
       CourseCard
     },
     data(){
       return {
-
+        courses: []
       }
     },
     created() {
+      this.current_user = this.$store.state.user.current_user
+      this.getInstructorCourses()
     },
     methods: {
+      async getInstructorCourses() {
+        let response = await CourseAPI.getInstructorCourses(this.current_user._id)
+        this.courses = response.data
+      }
     }
   }
 </script>
 
 <style scoped>
-
+  .course-list {
+    border: black solid;
+  }
 </style>
