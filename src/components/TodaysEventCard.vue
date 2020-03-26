c3451403957f374570adb46e253c206cca325831
Numfor Tiapo <numsmt2@gmail.com>
2020-03-25 23:30:05 -0400

    Add TodaysEventCard

---
+++ src/components/TodaysEventCard.vue
@@ -0,0 +1,30 @@
+<<template>
+  <div class="todays-event-card-container">
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'TodaysEventCard',
+    components: {
+    },
+    data(){
+      return {
+      }
+    },
+    created() {
+    },
+    methods: {
+    }
+  }
+</script>
+
+<style scoped>
+  .todays-event-card-container {
+    border: black solid;
+    height: 3.5rem;
+    margin-top: 1rem;
+    border-radius: 5px;
+  }
+</style>
