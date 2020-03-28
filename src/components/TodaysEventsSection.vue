101c0cf6c741abc28cce26260062a878bd9833b7
Numfor Tiapo <numsmt2@gmail.com>
2020-03-27 22:46:29 -0400

    Make TodaysEventsSection horizontally scrollable

--- src/components/TodaysEventsSection.vue
+++ src/components/TodaysEventsSection.vue
@@ -1,15 +1,20 @@
 <<template>
-  <div class="todays-events-section">
-    <div class="time-line">
-    </div>
-    <div class="todays-event-container" style="margin-left:1rem;">
-      <div class="todays-event-time">10a</div>
-      <TodaysEventCard />
-    </div>
-    <div class="todays-event-container" style="margin-left:4rem;">
-      <div class="todays-event-time">12p</div>
-      <TodaysEventCard />
-    </div>
+  <div>
+    <div class="todays-events-section">
+      <div class="todays-event-container" style="margin-left:1rem;">
+        <div class="todays-event-time">10a</div>
+        <TodaysEventCard />
+      </div>
+      <div class="todays-event-container" style="margin-left:4rem;">
+        <div class="todays-event-time">12p</div>
+        <TodaysEventCard />
+      </div>
+      <div class="todays-event-container" style="margin-left:4rem;">
+        <div class="todays-event-time">2p</div>
+        <TodaysEventCard />
+      </div>
+      </div>
+    <div class="time-line"></div>
   </div>
 </template>
 
@@ -35,20 +40,32 @@
 <style scoped>
 .todays-events-section {
   /*border: blue solid;*/
-  height: 4.5rem;
+  height: 9.5rem;
+  overflow-x: auto;
+  overflow-y: hidden;
+  white-space: nowrap;
+}
+
+.todays-events-section::-webkit-scrollbar {
+  width: 12px;
+}
+
+.todays-events-section::-webkit-scrollbar-thumb {
+border-radius: 10px;
+-webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.1);
+background-color: #F5F5F5; 
 }
 
 .time-line {
   border: #f0f0f0 solid 2px;
   background-color: #f0f0f0;
-  margin-top: 2rem;
+  margin-top: -7.7em;
 }
 
 .todays-event-container {
   /*border: red solid;*/
   height: 5rem;
   width: 10rem;
-  margin-top: -2rem;
   display: inline-block;
   vertical-align: top;
 }
