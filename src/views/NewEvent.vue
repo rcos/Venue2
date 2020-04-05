6c94e80f8936b66332403404fea0f203c38c799e
Numfor Tiapo <numsmt2@gmail.com>
2020-04-04 20:39:10 -0400

    Add basic inputs to NewEvent view

--- src/views/NewEvent.vue
+++ src/views/NewEvent.vue
@@ -1,9 +1,36 @@
 <template>
   <!-- ADDING USER -->
   <div>
-    <h2>Create An Event</h2>
-    <form @submit.prevent="addEvent">
-      <div class="row">
+    <h2>Create An Event For {{ course.name }}</h2>
+    <form class="new-event-form" @submit.prevent="addEvent">
+      <div class="form-group">
+        <div class="input-wrapper">
+          <label>Title</label>
+          <input type="text" class="form-control new-event-input" placeholder="e.g. Event 1" v-model="event.title">
+        </div>
+        <div class="input-wrapper">
+          <label>Location</label>
+          <input type="text" class="form-control new-event-input" placeholder="e.g. DCC 308" v-model="event.location">
+        </div>
+        <div class="input-wrapper">
+          <label>Start Time</label>
+          <datetime class="time-picker" type="datetime" use12-hour value-zone="local" title="Event Start" v-model="event.start_time"></datetime>
+          <label>End Time</label>
+          <datetime class="time-picker" type="datetime" use12-hour value-zone="local" title="Event End" v-model="event.end_time"></datetime>
+        </div>
+        <div class="input-wrapper" id="submission-time-wrapper">
+          <label>Submission Start Time</label>
+          <datetime class="time-picker" type="datetime" use12-hour value-zone="local" title="Submission Start" v-model="event.submission_start_time"></datetime>
+          <label>Submission End Time</label>
+          <datetime class="time-picker" type="datetime" use12-hour value-zone="local" title="Submission End" v-model="event.submission_end_time"></datetime>
+        </div>
+        <div class="input-wrapper">
+          <label>Section</label>
+          <input type="text" class="form-control new-event-input" v-model="section.number" readonly>
+        </div>
+
+      </div>
+<!--       <div class="row">
         <div class="col-md-6">
           <div class="form-group">
             <label>title:</label>
@@ -39,7 +66,7 @@
         </div><br />
         <div class="form-group">
           <button class="btn btn-primary">Create</button>
-        </div>
+        </div> -->
     </form>
 
     <!-- <Sections v-on:select-section="selectSection" /> -->
@@ -49,6 +76,7 @@
 
 <script>
   import EventAPI from '@/services/EventAPI.js';
+  import CourseAPI from '@/services/CourseAPI.js';
   // import Sections from '../Section/Sections';
   import { Datetime } from 'vue-datetime';
   import 'vue-datetime/dist/vue-datetime.css'
@@ -61,14 +89,20 @@
     },
     data(){
       return {
-        event: {
-        },
+        course: {},
+        event: {},
         section: {}
       }
     },
     created() {
+      this.getCourse()
     },
     methods: {
+      async getCourse() {
+        let course_id = this.$route.params.course_id
+        const response = await CourseAPI.getCourse(course_id)
+        this.course = response.data
+      },
       selectSection(section) {
         this.section = section
       },
@@ -82,3 +116,27 @@
     }
   }
 </script>
+
+<style>
+  .new-event-form {
+    /*border: black solid;*/
+  }
+
+  .input-wrapper {
+    width: 50%;
+    margin: auto;
+    margin-top: 3rem;
+    /*border: blue solid;*/
+  }
+
+  #submission-time-wrapper {
+    width: 80%;
+  }
+
+  .time-picker {
+    /*border: black solid;*/
+    display: inline-block;
+    margin-left: 1rem;
+    margin-right: 1rem;
+  }
+</style>
