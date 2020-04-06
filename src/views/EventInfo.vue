c71b5cfe1a71fcba46ea62bfbd001473230ff021
Numfor Tiapo <numsmt2@gmail.com>
2020-04-06 19:02:24 -0400

    Add EventInfo Component

---
+++ src/views/EventInfo.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    Hi I'm event info
+  </div>
+</template>
+
+<script>
+  import EventAPI from '@/services/EventAPI.js';
+
+export default {
+  name: 'EventInfo',
+  props: {
+  },
+  computed: {
+  },
+  components: {
+  },
+  data(){
+    return {
+
+    }
+  },
+  created() {
+
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped>
+</style>
