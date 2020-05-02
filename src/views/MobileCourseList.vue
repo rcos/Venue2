0a0003aaf72f22d8d3b203daae515f933bb32484
Abdul <muiz.rpi@gmail.com>
2020-05-02 15:56:19 -0400

    Made course list mobile view more mobile compatible

--- src/views/MobileCourseList.vue
+++ src/views/MobileCourseList.vue
@@ -1,6 +1,10 @@
 <template>
     <div>
-        <CourseList />
+        <div class="course-title-area">
+            <div class="course-title">Course Info</div>
+            <div class="course-load-count" v-if="course_count > 0">{{course_count}} Courses Loaded</div>
+        </div>
+        <CourseList :sizeCallback="setCourseSize"/>
         <show-at breakpoint="large">
             <!-- Add a component here that only shows when on desktop view, and i
             it calls a function on mount that redirects to /dashboard -->
@@ -24,11 +28,19 @@
             CourseList,
             showAt
         },
+        data(){
+            return {
+                course_count: 0
+            }
+        },
         methods: {
             visibilityChanged (is_visible, entry) {
                 if (is_visible) {
                     this.redirectToDashboard()
                 }
+            },
+            setCourseSize (course_count) {
+                this.course_count = course_count
             },
             redirectToDashboard () {
                 console.log(`Should redirect to dashboard router.`)
@@ -38,5 +50,25 @@
     }
 </script>
 <style>
+    .course-title-area {
+        text-align: left;
+        width: 80%;
+        margin: 4rem auto 0 auto;
+    }
+    
+    .course-title-area .course-title {
+        font-weight: bold;
+        font-size: 1.5rem;
+        color: #2c3e50;
+    }
 
+    .course-load-count {
+        color: rgba(87, 140, 171, 0.8);
+        font-size: 1.2rem;
+        margin-left: 0.9rem;
+    }
+
+    .course-title-area div {
+        display: inline-block;
+    }
 </style>
