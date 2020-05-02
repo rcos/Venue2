bcee29d5e4ffef8c6a91f9194a46983f45f0ee06
Abdul <muiz.rpi@gmail.com>
2020-05-02 15:13:00 -0400

    Made /user_courses redirect to /dashboard when on desktop view

---
+++ src/views/MobileCourseList.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <CourseList />
+        <show-at breakpoint="large">
+            <!-- Add a component here that only shows when on desktop view, and i
+            it calls a function on mount that redirects to /dashboard -->
+            <div v-observe-visibility="visibilityChanged">
+                <p>On Desktop View</p>
+            </div>
+        </show-at>
+    </div>
+</template>
+<script>
+    import CourseList from '@/components/CourseList'
+    import { ObserveVisibility } from 'vue-observe-visibility'
+    import {showAt} from 'vue-breakpoints'
+
+    export default {
+        name: 'MobileCourseList',
+        directives: {
+            ObserveVisibility
+        },
+        components: {
+            CourseList,
+            showAt
+        },
+        methods: {
+            visibilityChanged (is_visible, entry) {
+                if (is_visible) {
+                    this.redirectToDashboard()
+                }
+            },
+            redirectToDashboard () {
+                console.log(`Should redirect to dashboard router.`)
+                this.$router.push({ name: 'dashboard' })
+            }
+        }
+    }
+</script>
+<style>
+
+</style>
