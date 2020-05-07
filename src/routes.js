c8a80d23b6802611dd05e1aa93081aea80f29edd
Numfor Tiapo <numsmt2@gmail.com>
2020-05-07 16:43:23 -0400

    Revert "Created settings page"

--- src/routes.js
+++ src/routes.js
@@ -21,7 +21,6 @@ import Submissions from './components/admin/Submission/Submissions.vue';
 import LandingPage from './views/LandingPage.vue';
 import Signup from './components/Signup.vue';
 import Dashboard from './views/Dashboard.vue';
-import Settings from './views/Settings.vue';
 import CourseList from './components/CourseList.vue';
 import CourseInfo from './views/CourseInfo.vue';
 import NewEvent from './views/NewEvent.vue';
@@ -136,11 +135,6 @@ const router = new VueRouter({
       name: 'dashboard',
       path: '/dashboard',
       component: Dashboard,
-      meta: { requiresAuth: true }
-    },{
-      name: 'settings',
-      path: '/settings',
-      component: Settings,
       meta: { requiresAuth: true }
     },
     {
