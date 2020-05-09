973311caf9412b717cd5735cfcbbe8601bae0d6b
Ethan Whitton <ethanwhitton@snowmobility.com>
2020-05-09 15:24:02 -0400

    Lecture.route and LectureAPI set-up

---
+++ server/Lecture/Lecture.route.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const lectureRoutes = express.Router();
+
+let Lecture = require('../Lecture/Lecture.model')
+
+lectureRoutes.route('/add').post(function (req, res) {
+	let lecture = new Lecture(req.body.lecture);
+	lecture.save()
+		.then(() => {
+			res.status(200).json(lecture);
+		})
+		.catch(() => {
+			res.status(400).send("unable to save lecture to database");
+		});
+});
+
+lectureRoutes.route('/').get(function (req, res) {
+	Lecture.find(function (err, lectures) {
+		if (err)
+			res.json(err);
+		res.json(lectures);
+	});
+});
