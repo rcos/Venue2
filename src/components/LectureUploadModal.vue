64ee677295c6bd86990ff6f54af8d315021af4fd
Ethan Whitton <ethanwhitton@snowmobility.com>
2020-05-09 18:41:01 -0400

    lectureuploadmodal

---
+++ src/components/LectureUploadModal.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="lecture-upload-modal">
+    <input id="video_selector" type="file" />
+    <button id="video_upload_btn" class="btn btn-secondary" disabled>Upload</button>
+  </div>
+</template>
+
+<script>
+//imports here
+export default {
+  name: "LectureUploadModal",
+  props: {},
+  computed: {},
+  components: {},
+  data() {
+    return {};
+  },
+  created() {},
+  mounted() {
+    let vid_selector = document.getElementById("video_selector");
+    let vid_upload_btn = document.getElementById("video_upload_btn");
+    vid_selector.addEventListener("change", function() {
+      if (vid_selector.files.length == 0) {
+        vid_upload_btn.setAttribute("disabled");
+      } else {
+        vid_upload_btn.removeAttribute("disabled");
+      }
+    });
+  },
+  methods: {}
+};
+</script>
+
+<style scoped>
+</style>
