1c5ea36d452d4a43290428890ab884496525e52c
Ethan Whitton <ethanwhitton@snowmobility.com>
2020-05-12 15:38:42 -0400

    videojs install, instructor video preview window, basic poll creation UI

--- src/components/LectureUploadModal.vue
+++ src/components/LectureUploadModal.vue
@@ -1,21 +1,59 @@
 <template>
   <div id="lecture-upload-modal">
-    <input id="video_selector" name="lecturevideo" type="file" />
-    <button id="video_upload_btn" class="btn btn-secondary" @click="addLecture" disabled>Upload</button>
+    <button class="btn btn-primary" @click="handleShowModal">Upload lecture video</button>
+    <div v-if="showModal" id="lecture_modal_viewable">
+      <div class="row">
+          <button id="close_lecture_modal" class="btn btn-secondary" @click="showModal = false">X</button>
+      </div>
+      <input id="video_selector" name="lecturevideo" type="file" />
+      <button id="video_upload_btn" class="btn btn-secondary" @click="addLecture" disabled>Upload</button>
+      <div class="row" id="lecture_container" v-if="file_selected">
+        <div class="col-8" id="preview">
+          <video
+            id="video_player"
+            class="video-js"
+            controls
+            preload="auto"
+            poster="//vjs.zencdn.net/v/oceans.png"
+            data-setup='{}'>
+            <source src="//vjs.zencdn.net/v/oceans.mp4" type="video/mp4" />
+            <source src="//vjs.zencdn.net/v/oceans.webm" type="video/webm" />
+            <source src="//vjs.zencdn.net/v/oceans.ogv" type="video/ogg" />
+            <p class="vjs-no-js">
+              To view this video please enable JavaScript, and consider upgrading to a
+              web browser that
+              <a href="http://videojs.com/html5-video-support/" target="_blank">
+                supports HTML5 video
+              </a>
+            </p>
+          </video>
+        </div>
+        <div class="col">
+          <div v-for="(poll,i) in polls" v-bind:key="i">
+            <PollCard/>
+          </div>
+          <button id="add_poll_btn" class="btn btn-primary" @click="polls.push({})">+</button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import LectureAPI from "../services/LectureAPI";
+import PollCard from "./PollCard";
 // accept="video/*"
 export default {
   name: "LectureUploadModal",
   props: ["event"],
   computed: {},
-  components: {},
+  components: {PollCard},
   data() {
     return {
-      lecture: {}
+      lecture: {},
+      file_selected: false,
+      showModal: false,
+      polls: []
     };
   },
   created() {
@@ -24,16 +62,6 @@ export default {
     this.lecture.video_ref = "/videos/" + this.lecture.event + "/";
   },
   mounted() {
-    let vid_selector = document.getElementById("video_selector");
-    let vid_upload_btn = document.getElementById("video_upload_btn");
-
-    vid_selector.addEventListener("change", function() {
-      if (vid_selector.files.length == 0) {
-        vid_upload_btn.setAttribute("disabled");
-      } else {
-        vid_upload_btn.removeAttribute("disabled");
-      }
-    });
   },
   methods: {
     async addLecture() {
@@ -41,10 +69,63 @@ export default {
         this.lecture,
         document.getElementById("video_selector").files[0]
       );
+    },
+    handleShowModal() {
+      this.showModal = true
+      this.$nextTick(() => {
+        let vid_selector = document.getElementById("video_selector");
+        let vid_upload_btn = document.getElementById("video_upload_btn");
+        let self = this;
+        vid_selector.addEventListener("change", function() {
+          if (vid_selector.files.length == 0) {
+            vid_upload_btn.setAttribute("disabled");
+            self.file_selected = false;
+          } else {
+            vid_upload_btn.removeAttribute("disabled");
+            self.file_selected = true;
+          }
+        });
+      })
+    },
+    handleHideModal() {
+      this.showModal = false
+    },
+    handleNewPoll() {
+      
     }
   }
 };
 </script>
 
 <style scoped>
+#lecture_modal_viewable {
+  position: absolute;
+  background: white;
+  border: 1px solid black;
+  top: 100px;
+  bottom: 100px;
+  left: 100px;
+  right: 100px;
+}
+#close_lecture_modal {
+  position: absolute;
+  width: 30px;
+  margin: 0;
+  margin-left: 15px;
+  padding: 0;
+}
+#lecture_container {
+  margin: 0;
+  width: 100%;
+  position: absolute;
+  top: 50px;
+  bottom: 0px;
+}
+#video_player {
+  width: 100%;
+  height: auto;
+}
+#add_poll_btn {
+  width: 100%;
+}
 </style>
---
+++ src/components/PollCard.vue
@@ -0,0 +1,80 @@
+<template>
+	<div class="poll_card">
+		<div class="row questionrow">
+			Question: <input class="question" type="text"/>
+		</div>
+		<div v-for="(possible_answer, i) in possible_answers" v-bind:key="i" class="row">
+			<div class="col-1 answerpart">
+				{{i + 1}}
+			</div>
+			<div class="col answerpart">
+				<input class="answerfield" type="text"/>
+			</div>
+			<div class="col-1 answerpart">
+				<input type="checkbox"/>
+			</div>
+		</div>
+		<button id="add_answer_btn" class="btn" @click="possible_answers.push('')">+</button>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "PollCard",
+	props: ["timestamp"],
+	computed: {},
+	components: {},
+	data() {
+		return {
+			question: "",
+			is_mult_coice: true,
+			possible_answers: [],
+			is_correct: []
+		};
+	},
+	created() {
+		this.timestamp = this.$props.timestamp
+	},
+	mounted() {},
+	methods: {}
+}
+</script>
+
+<style scoped>
+.poll_card {
+	margin-bottom: 15px;
+	background: white;
+	border: 2px solid black;
+	padding: 10px;
+	width: 100%;
+}
+.answer {
+	margin-left: 15px;
+}
+.answerfield {
+	margin-left: 10px;
+	width: 100%;
+}
+#add_answer_btn {
+	margin-top: 10px;
+	width: 100%;
+}
+#add_answer_btn:hover {
+	border: 1px solid black;
+}
+.answerpart {
+	margin-left: 5px;
+	margin-right: 5px;
+	padding: 0;
+}
+.questionrow {
+	padding-left: 15px;
+}
+.question {
+	margin-left: 15px;
+}
+.row {
+	margin-top: 5px;
+	margin-bottom: 5px;
+}
+</style>
