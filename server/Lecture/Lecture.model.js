a160b5c25072aebb507c39a25e589a40202666d5
Numfor Tiapo <numsmt2@gmail.com>
2020-05-14 16:10:56 -0400

    Update Lecture Model

--- server/Lecture/Lecture.model.js
+++ server/Lecture/Lecture.model.js
@@ -1,24 +1,22 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-let Event = require('../Event/Event.model');
+let Section = require('../Section/Section.model');
 
 //Define collection and schema for Lecture
 let Lecture = new Schema({
-	event: {
+	section: {
 		type: mongoose.Schema.Types.ObjectId,
-		ref: 'Event'
+		ref: 'Section'
 	},
-	title: String,
-	time: Date,
-	is_active: {
-		type: Boolean,
-		default: false
-	},
+	allow_live_submissions: { type: Boolean, default: false },
+	allow_playback_submissions: { type: Boolean, default: false },
 	start_time: Date,
 	end_time: Date,
 	submission_start_time: Date,
 	submission_end_time: Date,
+	playback_submission_start_time: Date,
+	playback_submission_end_time: Date,
 	submission_window_status: {
 		is_pending: { type: Boolean, default: false },
 		is_ongoing: { type: Boolean, default: false },
@@ -27,7 +25,8 @@ let Lecture = new Schema({
 	video_ref: {
 		type: String,
 		default: ""
-	}
+	},
+	num_playback_polls: Number
 });
 
 module.exports = mongoose.model('Lecture', Lecture);
