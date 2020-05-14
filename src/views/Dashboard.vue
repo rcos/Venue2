946219682e78dd3de64d740440c3f7f934f6b663
Numfor Tiapo <numsmt2@gmail.com>
2020-05-14 17:16:48 -0400

    Move DashboardSection divs to Dashboard

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,7 +1,17 @@
 <template>
   <div>
-    <DashboardSection active_section/>
-    <DashboardSection today_section/>
+<!--     <DashboardSection active_section/>
+    <DashboardSection today_section/> -->
+    <div class="dashboard-section" id="section-1">
+      <h4 class="section-title">Live</h4>
+      
+    </div>
+
+    <div class="dashboard-section" id="section-2">
+      <h4 class="section-title">Playback</h4>
+      
+    </div>
+
     <hide-at breakpoint="mediumAndBelow">
       <DashboardSection courses_section/>
     </hide-at>
@@ -42,3 +52,30 @@
     }
   }
 </script>
+
+<style scoped>
+  .dashboard-section {
+    /*border: red solid;*/
+    text-align: left;
+    margin-top: 4rem;
+    display: inline-block;
+    vertical-align: top;
+    height: 15rem;
+  }
+
+  #section-1 {
+    float: left;
+    margin-left: 8rem;
+    width: 30rem;
+  }
+
+  #section-2 {
+    width: 30rem;
+    margin-left: -5rem;
+  }
+  
+  .section-title {
+    font-weight: bold;
+  }
+
+</style>
