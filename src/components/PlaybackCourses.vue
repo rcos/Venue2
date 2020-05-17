3cd3ba9dc8e7cfa86d06482c09c5dec11dd92680
Abdul <muiz.rpi@gmail.com>
2020-05-17 14:02:23 -0400

    Course Info, Course statistics and dashboard update

---
+++ src/components/PlaybackCourses.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="dashboard-section-modal">
+    <div class="title">Playback</div>
+
+    <div class="sub-section">
+
+    </div>
+  </div>
+</template>
+<script>
+
+  export default {
+    name: 'PlaybackCourses',
+    props: {
+      mobileMode: Boolean
+    },
+    data () {
+      return {}
+    },
+    created () {
+
+    },
+    methods: {
+
+    }
+  }
+
+</script>
