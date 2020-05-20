726d51eb76b5c30b0cae2ab8269383c077495406
Ethan Whitton <ethanwhitton@snowmobility.com>
2020-05-19 20:36:04 -0400

    seeding fix for hashing passwords and removed event and submissions

--- server/seed.js
+++ server/seed.js
@@ -5,8 +5,13 @@ const Section = require("./Section/Section.model")
 const Submission = require("./Submission/Submission.model")
 const User = require("./User/User.model")
 const Lecture = require("./Lecture/Lecture.model")
+const LectureSubmission = require("./LectureSubmission/LectureSubmission.model")
+const PlaybackPoll = require("./PlaybackPoll/PlaybackPoll.model")
 
 const db = "mongodb://localhost:27017/Venue2";
+
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 seeder.connect(db, function () {
 	seeder.loadModels([
@@ -15,177 +20,133 @@ seeder.connect(db, function () {
 		"./Section/Section.model",
 		"./Event/Event.model",
 		"./Submission/Submission.model",
-		"./Lecture/Lecture.model"
+		"./Lecture/Lecture.model",
+		"./LectureSubmission/LectureSubmission.model",
+		"./PlaybackPoll/PlaybackPoll.model"
 	]);
-	seeder.clearModels(['Course', 'User', 'Section', 'Event', 'Submission', 'Lecture'], function () {
-		seeder.populateModels(data, function (err, done) {
-			if (err) {
-				return console.log("seed err", err)
-			}
-			if (done) {
-				return console.log("seed finished", done)
-			}
-			seeder.disconnect()
+	seeder.clearModels(['Course', 'User', 'Section', 'Event', 'Submission', 'Lecture', 'LectureSubmission', 'PlaybackPoll'], function () {
+
+		let u0 = new User({
+			first_name: "Admin",
+			last_name: "Admin",
+			email: "admin@gmail.com",
+			password: "nimda",
+			is_instructor: true,
+			ta_sections: [],
+			submissions: []
 		})
+
+		let u1 = new User({
+			first_name: "Student",
+			last_name: "A",
+			email: "studenta@gmail.com",
+			password: "password",
+			is_instructor: false,
+			ta_sections: [],
+			submissions: []
+		})
+
+		let u2 = new User({
+			first_name: "Student",
+			last_name: "B",
+			email: "studentb@gmail.com",
+			password: "password",
+			is_instructor: false,
+			ta_sections: [],
+			submissions: []
+		})
+
+		let u3 = new User({
+			first_name: "Student",
+			last_name: "C",
+			email: "studentc@gmail.com",
+			password: "password",
+			is_instructor: false,
+			ta_sections: [],
+			submissions: []
+		})
+
+		let c0 = new Course({
+			name: "RCOS",
+			dept: "Computer Science",
+			course_number: 0,
+			instructor: u0._id
+		})
+
+		let c1 = new Course({
+			name: "Data Structures",
+			dept: "Computer Science",
+			course_number: 1,
+			instructor: u0._id
+		})
+
+		let c2 = new Course({
+			name: "Multiletiable Calculus",
+			dept: "Mathematics",
+			course_number: 2,
+			instructor: u0._id
+		})
+
+		let c3 = new Course({
+			name: "Physics I",
+			dept: "Science",
+			course_number: 3,
+			instructor: u0._id
+		})
+
+		let s0 = new Section({
+			course: c0._id,
+			number: 0,
+			students: [u1._id, u3._id],
+			teaching_assistants: [u0._id]
+		})
+
+		let s1 = new Section({
+			course: c1._id,
+			number: 0,
+			students: [u1._id, u2._id],
+			teaching_assistants: [u0._id]
+		})
+
+		let s2 = new Section({
+			course: c1._id,
+			number: 1,
+			students: [u1._id, u2._id],
+			teaching_assistants: [u0._id]
+		})
+
+		bcrypt.hash(u0.password, saltRounds, (err, hash) => {
+			u0.password = hash
+			bcrypt.hash(u1.password, saltRounds, (err, hash) => {
+				u1.password = hash
+				bcrypt.hash(u2.password, saltRounds, (err, hash) => {
+					u2.password = hash
+					bcrypt.hash(u3.password, saltRounds, (err, hash) => {
+						u3.password = hash
+
+						let data = [{
+							"model": "User",
+							"documents": [u0, u1, u2, u3]
+						}, {
+							"model": "Course",
+							"documents": [c0, c1, c2, c3]
+						}, {
+							"model": "Section",
+							"documents": [s0, s1, s2]
+						}]
+
+						seeder.populateModels(data, function (err, done) {
+							if (err) {
+								return console.log("seed err", err)
+							}
+							if (done) {
+								return console.log("seed finished", done)
+							}
+							seeder.disconnect()
+						})
+					});
+				});
+			});
+		});
 	});
 });
-
-var u0 = new User({
-	first_name: "Admin",
-	last_name: "Admin",
-	email: "admin@gmail.com",
-	password: "nimda",
-	is_instructor: true,
-	ta_sections: [],
-	submissions: []
-})
-
-var u1 = new User({
-	first_name: "Student",
-	last_name: "A",
-	email: "studenta@gmail.com",
-	password: "password",
-	is_instructor: false,
-	ta_sections: [],
-	submissions: []
-})
-
-var u2 = new User({
-	first_name: "Student",
-	last_name: "B",
-	email: "studentb@gmail.com",
-	password: "password",
-	is_instructor: false,
-	ta_sections: [],
-	submissions: []
-})
-
-var u3 = new User({
-	first_name: "Student",
-	last_name: "C",
-	email: "studentc@gmail.com",
-	password: "password",
-	is_instructor: false,
-	ta_sections: [],
-	submissions: []
-})
-
-var c0 = new Course({
-	name: "RCOS",
-	dept: "Computer Science",
-	course_number: 0,
-	instructor: u0._id
-})
-
-var c1 = new Course({
-	name: "Data Structures",
-	dept: "Computer Science",
-	course_number: 1,
-	instructor: u0._id
-})
-
-var c2 = new Course({
-	name: "Multivariable Calculus",
-	dept: "Mathematics",
-	course_number: 2,
-	instructor: u0._id
-})
-
-var c3 = new Course({
-	name: "Physics I",
-	dept: "Science",
-	course_number: 3,
-	instructor: u0._id
-})
-
-var s0 = new Section({
-	course: c0._id,
-	number: 0,
-	students: [u1._id, u3._id],
-	teaching_assistants: [u0._id]
-})
-
-var s1 = new Section({
-	course: c1._id,
-	number: 0,
-	students: [u1._id, u2._id],
-	teaching_assistants: [u0._id]
-})
-
-var s2 = new Section({
-	course: c1._id,
-	number: 1,
-	students: [u1._id, u2._id],
-	teaching_assistants: [u0._id]
-})
-
-var e0 = new Event({
-	title: "Large Group",
-	section: s0._id,
-	is_active: true,
-	start_time: Date(),
-	end_time: Date() + (2 * 60 * 60 * 1000),
-	geofence: []
-	// time_range: null
-})
-
-var e1 = new Event({
-	title: "Small Group",
-	section: s0._id,
-	is_active: false,
-	start_time: Date(),
-	end_time: Date() + (2 * 60 * 60 * 1000),
-	geofence: []
-	// time_range: null
-})
-
-var e2 = new Event({
-	title: "Small Group",
-	section: s0._id,
-	is_active: false,
-	start_time: Date(),
-	end_time: Date() + (2 * 60 * 60 * 1000),
-	geofence: []
-	// time_range: null
-})
-
-var e3 = new Event({
-	title: "Small Group",
-	section: s0._id,
-	is_active: false,
-	start_time: Date(),
-	end_time: Date() + (2 * 60 * 60 * 1000),
-	geofence: []
-	// time_range: null
-})
-
-var b0 = new Submission({
-	event: e0._id,
-	submitter: u1._id,
-	time: Date(),
-	is_verified: false
-})
-
-var b1 = new Submission({
-	event: e0._id,
-	submitter: u2._id,
-	time: Date(),
-	is_verified: true
-})
-
-const data = [{
-	"model": "User",
-	"documents": [u0, u1, u2, u3]
-}, {
-	"model": "Course",
-	"documents": [c0, c1, c2, c3]
-}, {
-	"model": "Section",
-	"documents": [s0, s1, s2]
-}, {
-	"model": "Event",
-	"documents": [e0, e1, e2, e3]
-}, {
-	"model": "Submission",
-	"documents": [b0, b1]
-}]
