b68150f8b6056402e6dd59af4eb7696b4e24838b
Ethan Whitton <ejwhitton43@gmail.com>
2020-05-21 16:17:12 -0400

    Console logging for PlaybackPoll routes

--- server/PlaybackPoll/PlaybackPoll.route.js
+++ server/PlaybackPoll/PlaybackPoll.route.js
@@ -7,9 +7,11 @@ pollRoutes.route('/add').post(function (req, res) {
   let poll = new PlaybackPoll(req.body.poll);
   poll.save()
     .then(() => {
+      console.log("<SUCCESS> Adding poll:",poll)
       res.status(200).json(poll);
     })
     .catch(() => {
+      console.log("<ERROR> Adding poll:",poll)
       res.status(400).send("unable to save poll to database");
     });
 });
@@ -17,8 +19,10 @@ pollRoutes.route('/add').post(function (req, res) {
 pollRoutes.route('/').get(function (req, res) {
   PlaybackPoll.find(function (err, polls) {
     if (err || polls == null) {
+      console.log("<ERROR> Getting all polls")
       res.json(err);
     } else {
+      console.log("<SUCCESS> Getting all polls")
       res.json(polls);
     }
   });
@@ -29,8 +33,10 @@ pollRoutes.route('/by_lecture/:lectureid').get(function (req, res) {
     {lecture: req.params.lectureid},
     function (err, polls) {
       if (err || polls == null) {
+        console.log("<ERROR> Getting polls for lecture with ID:",req.params.lectureid)
         res.json(err);
       } else {
+        console.log("<SUCCESS> Getting polls for lecture with ID:",req.params.lectureid)
         res.json(polls);
       }
     }
