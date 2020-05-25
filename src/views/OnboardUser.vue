299fb06ebdc164c1c3b03d47e4b66d907d3f2b8e
Numfor Tiapo <numsmt2@gmail.com>
2020-05-25 14:00:57 -0400

    Add OnboardUser component

---
+++ src/views/OnboardUser.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <h2>Onboard User</h2>
+    <form @submit.prevent="addUser">
+      <div class="row">
+        <div class="col-md-6">
+          <div class="form-group">
+            <label>first_name:</label>
+            <input type="text" class="form-control" v-model="user.first_name">
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-6">
+          <div class="form-group">
+            <label>last_name:</label>
+            <input class="form-control" v-model="user.last_name">
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-6">
+          <div class="form-group">
+            <label>user_id:</label>
+            <input class="form-control" v-model="user.user_id">
+          </div>
+        </div>
+      </div>
+      <div>
+        is_instructor: <input type="checkbox" v-model="user.is_instructor">
+      </div><br/>
+      <div class="form-group">
+        <button class="btn btn-primary">Onboard</button>
+      </div>
+    </form>
+
+  </div>
+</template>
+
+<script>
+  import UserAPI from '@/services/UserAPI.js';
+
+  export default {
+    name: 'OnboardUser',
+    data(){
+      return {
+        user: {}
+      }
+    },
+    created() {
+    },
+    methods: {
+      async addUser(evt){
+        evt.preventDefault(); // prevents the form's default action from redirecting the page
+        const response = await UserAPI.addUser(this.user);
+        this.$router.push({name: 'users'});
+      },
+    }
+  }
+</script>
