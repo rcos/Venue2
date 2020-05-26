f588eb5bb77c899a3389f2674355ebc1d44bb8e4
Abdul <muiz.rpi@gmail.com>
2020-05-26 00:17:58 -0400

    Fixed CourseInfoTitle component course error

--- src/views/CourseInfo.vue
+++ src/views/CourseInfo.vue
@@ -11,7 +11,7 @@
         </router-link>
         </div>
         <div>
-          <CourseInfoTitle :course="course" class="inline-block" :section_number="this.current_user.is_instructor ? -1 : section.number" />
+          <CourseInfoTitle :course="typeof course == typeof {} ? course : {}" class="inline-block" :section_number="this.current_user.is_instructor ? -1 : section.number" />
 
           <!-- Lecture Pills -->
           <div class="lecture-pills-min-container inline-block">
