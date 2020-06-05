b384faee7f9203976568bdc83e896c7b330f5e44
Numfor Tiapo <numsmt2@gmail.com>
2020-06-05 19:41:25 -0400

    Add Lecture time info to LectureInfoHeader Component

--- src/components/LectureInfoHeader.vue
+++ src/components/LectureInfoHeader.vue
@@ -1,14 +1,68 @@
 <template>
   <div id="lecture-info-section">
     <h1>{{lecture.title}} Info</h1>
-
+    <show-at breakpoint="small">
+      <!-- Mobile Start -->
+      <div id="lecture-data">
+        <div class="row main-info">
+          <h3>{{lecture.sections[0].course.name}}</h3>
+          <h5 class="dept-and-number">{{lecture.sections[0].course.dept}} {{lecture.sections[0].course.course_number}}</h5>
+          <h5>Sections: <a class="section-numbers" v-for="(section,i) in lecture.sections" :key="i">{{ section.number }}</a></h5>
+        </div>
+        <div class="row">
+          <h5>Start Time:<h6>{{new Date(lecture.start_time)}}</h6></h5>
+        </div>
+        <div class="row">
+          <h5>End Time: <h6>{{new Date(lecture.end_time)}}</h6></h5>
+        </div>
+        <div class="row">
+          <h5>Submission Start Time: <h6>{{new Date(lecture.submission_start_time)}}</h6></h5>
+        </div>
+        <div class="row">
+          <h5>Submission End Time: <h6>{{new Date(lecture.submission_end_time)}}</h6></h5>
+        </div>
+      </div>
+      <!-- Mobile End -->
+    </show-at>
+    <show-at breakpoint="mediumAndAbove">
+      <!-- Not-mobile Start -->
+      <div class="row" id="lecture-data">
+        <div class="col main-info">
+          <h3>{{lecture.sections[0].course.name}}</h3>
+          <h5 class="dept-and-number">{{lecture.sections[0].course.dept}} {{lecture.sections[0].course.course_number}}</h5>
+          <h5>Sections: <a class="section-numbers" v-for="(section,i) in lecture.sections" :key="i">{{ section.number }}</a></h5>
+        </div>
+        <div class="col">
+          <h5>Start Time</h5>
+          <h6>{{new Date(lecture.start_time)}}</h6>
+        </div>
+        <div class="col">
+          <h5>End Time</h5>
+          <h6>{{new Date(lecture.end_time)}}</h6>
+        </div>
+        <div class="col">
+          <h5>Submission Start Time</h5>
+          <h6>{{new Date(lecture.submission_start_time)}}</h6>
+        </div>
+        <div class="col">
+          <h5>Submission End Time</h5>
+          <h6>{{new Date(lecture.submission_end_time)}}</h6>
+        </div>
+      </div>
+      <!-- Not-mobile End -->
+    </show-at>
   </div>
 </template>
 
 <script>
+  import {showAt, hideAt} from 'vue-breakpoints';
 
   export default {
     name: 'LectureInfoHeader',
+    components: {
+      showAt,
+      hideAt
+    },
     props: {
       lecture: Object
     },
@@ -17,7 +71,7 @@
       }
     },
     created() {
-
+      console.log("Received this lecture",this.lecture)
     },
     methods: {
     }
@@ -25,5 +79,40 @@
 </script>
 
 <style scoped>
+  #lecture-info-section {
+    position: relative;
+    border: black solid;
+  }
+
+  #lecture-data {
+    position: relative;
+    text-align: center;
+    margin-top: 3rem;
+    margin-left: 3rem;
+  }
+
+  .row {
+    position: relative;
+    right: 1rem;
+  }
+
+  .col.main-info {
+    border-radius: .5rem;
+    box-shadow: 0 5px 10px -1px gray;
+    padding: 1rem;
+  }
+
+  .row.main-info {
+    border-radius: .5rem;
+    box-shadow: 0 5px 10px -1px gray;
+    padding: 1rem;
+    margin-right: 1rem;
+  }
+
+  .dept-and-number {
+    background: black;
+    color: white;
+    border-radius: 1rem;
+  }
 
 </style>
