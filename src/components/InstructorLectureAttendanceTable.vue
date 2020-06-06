75f6c6a019f3a97ccfddd303d4eb35b2cb27fe68
Numfor Tiapo <numsmt2@gmail.com>
2020-06-06 12:21:30 -0400

    Show all lecture types in LectureAttendanceList

--- src/components/InstructorLectureAttendanceTable.vue
+++ src/components/InstructorLectureAttendanceTable.vue
@@ -6,47 +6,16 @@
       <button id="absent_btn" class="tab_btn" @click="selectTab(2)"><h5>Absent ({{absent.length}}/{{all_students.length}})</h5></button>
       <button id="stats_btn" class="tab_btn" @click="selectTab(3)"><h5>Statistics</h5></button>
     </div>
-    <div v-if="selected_tab == 0" id="live_submit" class="tab_section">
-      <div v-if="Object.keys(live_submissions).length > 0">
-        <div class="namecard-edging live-color" v-for="(submission,i) in Object.keys(live_submissions)" :key="i">
-          <div class="namecard">
-            {{live_submissions[submission][0].submitter.first_name}} {{live_submissions[submission][0].submitter.last_name}}
-            {{live_submissions[submission][0].submitter.email}}
-            {{live_submissions[submission].length / lecture.checkins.length * 100}}%
-          </div>
-        </div>
-      </div>
-      <div v-else>
-        None
-      </div>
+    <div v-if="selected_tab === 0" id="live_submit" class="tab_section">
+      <LectureAttendanceList :submissions="live_submissions" :is_live="true" />
     </div>
-    <div v-if="selected_tab == 1" id="playback_submit" class="tab_section">
-      <div v-if="playback_submissions.length > 0">
-        <div class="namecard-edging playback-color" v-for="(submission,i) in playback_submissions" :key="i">
-          <div class="namecard">
-            {{submission.submitter.first_name}} {{submission.submitter.last_name}}
-            {{submission.submitter.email}}
-          </div>
-        </div>
-      </div>
-      <div v-else>
-        None
-      </div>
+    <div v-if="selected_tab === 1" id="playback_submit" class="tab_section">
+      <LectureAttendanceList :submissions="playback_submissions" :is_playback="true" />
     </div>
-    <div v-if="selected_tab == 2" id="no_submit" class="tab_section">
-      <div v-if="absent.length > 0">
-        <div class="namecard-edging absent-color" v-for="(absentee,i) in absent" :key="i">
-          <div class="namecard">
-            {{absentee.first_name}} {{absentee.last_name}}
-            {{absentee.email}}
-          </div>
-        </div>
-      </div>
-      <div v-else>
-        None
-      </div>
+    <div v-if="selected_tab === 2" id="no_submit" class="tab_section">
+      <LectureAttendanceList :submissions="absent" :is_absent="true" />
     </div>
-    <div v-if="selected_tab == 3" id="stats" class="tab_section">
+    <div v-if="selected_tab === 3" id="stats" class="tab_section">
       Statistics
     </div>
 		</div>
@@ -55,6 +24,7 @@
 
 
 <script>
+  import LectureAttendanceList from '@/components/LectureAttendanceList.vue'
 
   export default {
     name: 'InstructorLectureAttendanceTable',
@@ -65,14 +35,14 @@
     	all_students: Array
     },
     components: {
+      LectureAttendanceList
     },
     data(){
       return {
-        selected_tab: 0
+        selected_tab: 1
       }
     },
     created() {
-
     },
     methods: {
     	selectTab(i) {
@@ -129,44 +99,4 @@
 	  height: 17rem;
 	  padding-bottom: 3rem;
 	}
-
-	.namecard-edging {
-	  display: inline-block;
-	  border-radius: .25rem;
-	  width: 12rem;
-	  height: 4rem;
-	  margin-right: 1rem;
-	  margin-bottom: 1rem;
-	}
-
-	.namecard-edging.live-color {
-	  background: green;
-	}
-
-	.namecard-edging.playback-color {
-	  background: blueviolet;
-	}
-
-	.namecard-edging.absent-color {
-	  background: red;
-	}
-
-	.namecard {
-	  position: relative;
-	  background: white;
-	  text-align: center;
-	  border-radius: .25rem;
-	  top: 0.5rem;
-	  margin-left: 0.5rem;
-	  width: 11rem;
-	  height: 4rem;
-	  box-shadow: 0 5px 10px -1px gray;
-	  padding-top: 0.5rem;
-	  z-index: 100;
-	}
-
-	.hidden {
-	  display: none;
-	}
-
 </style>
---
+++ src/components/LectureAttendanceList.vue
@@ -0,0 +1,115 @@
+<template>
+	<div>
+    <div v-if="is_live">
+      <div v-if="Object.keys(submissions).length > 0">
+        <div class="namecard-edging live-color" v-for="(submission,i) in Object.keys(submissions)" :key="i">
+          <div class="namecard">
+            {{submissions[submission][0].submitter.first_name}} {{submissions[submission][0].submitter.last_name}}
+            {{submissions[submission][0].submitter.email}}
+            {{submissions[submission].length / lecture.checkins.length * 100}}%
+          </div>
+        </div>
+      </div>
+      <div v-else>
+        None
+      </div>
+    </div>
+    <div v-else-if="is_playback">
+      <div v-if="submissions.length > 0">
+        <div class="namecard-edging playback-color" v-for="(submission,i) in submissions" :key="i">
+          <div class="namecard">
+            {{submission.submitter.first_name}} {{submission.submitter.last_name}}
+            {{submission.submitter.email}}
+          </div>
+        </div>
+      </div>
+      <div v-else>
+        None
+      </div>
+    </div>
+    <div v-else-if="is_absent">
+      <div v-if="submissions.length > 0">
+        <div class="namecard-edging absent-color" v-for="(absentee,i) in submissions" :key="i">
+          <div class="namecard">
+            {{absentee.first_name}} {{absentee.last_name}}
+            {{absentee.email}}
+          </div>
+        </div>
+      </div>
+      <div v-else>
+        None
+      </div>
+    </div>
+	</div>
+</template>
+
+
+<script>
+
+  export default {
+    name: 'LectureAttendanceList',
+    props: {
+      submissions: Array,
+      is_live: {
+        type: Boolean,
+        default: false
+      },
+      is_playback: {
+        type: Boolean,
+        default: false
+      },
+      is_absent: {
+        type: Boolean,
+        default: false
+      }
+    },
+    components: {
+    },
+    data(){
+      return {
+        selected_tab: 0
+      }
+    },
+    created() {
+    },
+    methods: {
+    }
+  }
+</script>
+
+<style scoped>
+	.namecard-edging {
+	  display: inline-block;
+	  border-radius: .25rem;
+	  width: 12rem;
+	  height: 4rem;
+	  margin-right: 1rem;
+	  margin-bottom: 1rem;
+	}
+
+	.namecard-edging.live-color {
+	  background: green;
+	}
+
+	.namecard-edging.playback-color {
+	  background: blueviolet;
+	}
+
+	.namecard-edging.absent-color {
+	  background: red;
+	}
+
+	.namecard {
+	  position: relative;
+	  background: white;
+	  text-align: center;
+	  border-radius: .25rem;
+	  top: 0.5rem;
+	  margin-left: 0.5rem;
+	  width: 11rem;
+	  height: 4rem;
+	  box-shadow: 0 5px 10px -1px gray;
+	  padding-top: 0.5rem;
+	  z-index: 100;
+	}
+</style>
