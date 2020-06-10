afd9216e477c165e49db2b9db46663101e1184cf
Ethan Whitton <ejwhitton43@gmail.com>
2020-06-10 19:48:36 -0400

    prevent 'button' from showing twice for screen readers

--- src/components/InstructorLectureAttendanceContainer.vue
+++ src/components/InstructorLectureAttendanceContainer.vue
@@ -2,15 +2,15 @@
 	<div>
 		<div id="container-header">
 		  <h2>Attendance</h2>
-		  <button v-if="lecture.lecture_status === 'is_live' && lecture.checkin_window_status === 'open'" @click="showQR" class="header-btn">Show QR</button>
+		  <button v-if="lecture.lecture_status === 'is_live' && lecture.checkin_window_status === 'open'" @click="showQR" class="header-btn btn btn-secondary">Show QR</button>
 		  <LectureUploadModal v-else-if="lecture.lecture_status === 'is_over' && !lecture.allow_playback_submissions" :lecture="lecture" :update_lecture="true" />
-		  <router-link v-else-if="lecture.allow_playback_submissions" :to="{name: 'lecture_playback', params: { lecture_id: lecture._id }}" aria-label="Watch Playback">
-		  	<button >Watch Playback</button>
+		  <router-link class="header-btn btn btn-secondary" v-else-if="lecture.allow_playback_submissions" :to="{name: 'lecture_playback', params: { lecture_id: lecture._id }}" aria-label="Watch Playback">
+		  	Watch Playback
 		  </router-link>
 	  </div>
 	  <div id="qr_modal" class="hidden">
 	    <canvas id="qr_render_area"></canvas>
-	    <button id="close_qr_btn" @click="hideQR" aria-label="Hide QR">Hide</button>
+	    <button class="btn btn-secondary" id="close_qr_btn" @click="hideQR" aria-label="Hide QR">Hide</button>
 	  </div>
 	  <LectureAttendanceTable :is_instructor="true" :lecture="lecture" :live_submissions="live_submissions" :playback_submissions="playback_submissions" :absent="absent" :all_students="all_students" />
 	</div>
