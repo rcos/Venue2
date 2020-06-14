770966d1433891a7459caa948dac32b74a74485d
Numfor Tiapo <numsmt2@gmail.com>
2020-06-14 14:46:40 -0400

    Split up LectureCard into InstructorLectureCard and StudentLectureCard

---
+++ src/components/InstructorLectureCard.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="lecture-card">
+    I'm the instructor
+      
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'LectureCard',
+    props: {
+      lecture_type: String
+    },
+    computed: {
+    },
+    components: {
+
+    },
+    data(){
+      return {
+      }
+    },
+    created() {
+
+    },
+    methods: {
+
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/StudentLectureCard.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="lecture-card">
+    I'm the student
+      
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'LectureCard',
+    props: {
+      lecture_type: String
+    },
+    computed: {
+    },
+    components: {
+
+    },
+    data(){
+      return {
+      }
+    },
+    created() {
+
+    },
+    methods: {
+
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
