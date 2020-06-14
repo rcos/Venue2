917de99a28974ea16460405d541118f2fefd9c62
Numfor Tiapo <numsmt2@gmail.com>
2020-06-14 19:55:40 -0400

    Change Dashboard to InstructorDashboard or StudentDashboard in NavBar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -10,13 +10,15 @@
         <!-- Dashboard -->
         <div class="venue-nav-link-container" v-if="this.$route.name === 'dashboard'">
           <router-link class="venue-nav-link active-link" :to="{name: 'dashboard'}">
-            Dashboard
+            <p v-if="is_instructor">Instructor Dashboard</p>
+            <p v-else>Student Dashboard</p>
           </router-link>
           <div class="active-link-underline"></div>
         </div>
         <div v-else class="venue-nav-link-container">
           <router-link class="venue-nav-link" :to="{name: 'dashboard'}">
-            Dashboard
+            <p v-if="is_instructor">Instructor Dashboard</p>
+            <p v-else>Student Dashboard</p>
           </router-link>
         </div>
         <!-- Courses -->
@@ -77,7 +79,8 @@
     },
     data(){
       return {
-        current_user: {}
+        current_user: {},
+        is_instructor: Boolean
       }
     },
     created() {
@@ -86,6 +89,7 @@
     methods: {
       getCurrentUser() {
         this.current_user = this.$store.state.user.current_user
+        this.is_instructor = this.current_user.is_instructor
       },
     }
   }
