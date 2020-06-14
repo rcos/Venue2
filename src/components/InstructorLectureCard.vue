a692a050e91b92b6e2ad05427b42ff6e7b7f465f
Numfor Tiapo <numsmt2@gmail.com>
2020-06-14 16:11:36 -0400

    Fix Dashboard overflow

--- src/components/InstructorLectureCard.vue
+++ src/components/InstructorLectureCard.vue
@@ -1,8 +1,26 @@
 <template>
   <div class="lecture-card">
-    I'm the instructor
-      
+    <div class="lecture-card-section" id="left-section">
+      <p class="course-name">Open Source Software</p>
+      <p class="course-title">CSCI 2960-01</p>
     </div>
+    <div class="lecture-card-section" id="middle-section">
+      <p class="lecture-name">Class 5/20</p>
+    </div>
+    <div class="lecture-card-section" id="right-section"></div>
+
+<!--     <div v-if="lecture_type === 'Live'">
+      I'm live
+    </div>
+    <div v-else-if="lecture_type === 'Playback'">
+      Playback
+    </div>
+    <div v-else-if="lecture_type === 'Recent'">
+      Recent
+    </div>
+    <div v-else-if="lecture_type === 'Upcoming'">
+      Upcoming
+    </div> -->
   </div>
 </template>
 
@@ -22,7 +40,6 @@
       }
     },
     created() {
-
     },
     methods: {
 
