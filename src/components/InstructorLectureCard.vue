b404e574a28dc256be72ed20ad65be66768c9eaf
Numfor Tiapo <numsmt2@gmail.com>
2020-06-14 19:24:09 -0400

    Show time to window close on live lecture cards

--- src/components/InstructorLectureCard.vue
+++ src/components/InstructorLectureCard.vue
@@ -10,7 +10,7 @@
     <div class="lecture-card-section" id="right-section">
       <div v-if="lecture_type === 'Live'" class="right-container">
         <img src="@/assets/clock.svg" class="clock" aria-label="Time Icon">
-        <p v-if="lecture.checkin_window_status === 'open'" class="lecture-time-text open-text">5m</p>
+        <p v-if="lecture.checkin_window_status === 'open'" class="lecture-time-text open-text">{{ getTimeToWindowCloseString() }}</p>
         <p v-else class="lecture-time-text closed-text">closed</p>
       </div>
       <div v-else-if="lecture_type === 'Playback'" class="right-container">
@@ -46,6 +46,28 @@
     created() {
     },
     methods: {
+      getTimeToWindowCloseString() {
+        let current_time = new Date()
+        let window_close_time = this.getCurrentCheckinWindowCloseTime()
+        // get total seconds between the times
+        let delta = Math.abs(window_close_time - current_time) / 1000;
+        let diff_days = Math.floor(delta / 86400);
+        var diff_hours = Math.floor(delta / 3600) % 24;
+        var diff_minutes = Math.floor(delta / 60) % 60;
+        console.log("diff_days",diff_days)
+        console.log("diff_hours",diff_hours)
+        console.log("diff_minutes",diff_minutes)
+        let time_to_close_str = ""
+        if(diff_days > 0)
+          time_to_close_str += diff_days + "d "
+        if(diff_hours > 0)
+          time_to_close_str += diff_hours + "h "
+        time_to_close_str += diff_minutes + "m "
+        return time_to_close_str
+      },
+      getCurrentCheckinWindowCloseTime() {
+        return new Date(this.lecture.checkins[this.lecture.checkin_index].end_time)
+      },
       getUpcomingTimeString(time) {
         let upcoming_time = new Date(time)
         let month = upcoming_time.getMonth()
