7b4173096772b3d87676246886ed83f9df7c8fa1
TheStopsign <ethanwhitton@snowmobility.com>
2020-06-24 13:18:12 -0400

    add accessibility.spec file

---
+++ cypress/integration/accessibility.spec.js
@@ -0,0 +1,30 @@
+describe('Accessibility for Landing',function(){
+	beforeEach(() => {
+		cy.clearCookies()
+		cy.clearLocalStorage()
+	})
+	it('on the landing page',function(){
+		cy.visit('/')
+		cy.checkAccessibility()
+	})
+	it('in the login form',function(){
+		cy.get('.venue-btn').contains('Get Started').click()
+		cy.wait(500)
+		cy.checkAccessibility()
+	})
+})
+describe('Accessibility for Instructors',function(){
+	beforeEach(() => {
+		cy.clearCookies()
+		cy.clearLocalStorage()
+		cy.login('testinst','password')
+	})
+	it('on the dashboard page',function(){
+		cy.visit('/dashboard')
+		cy.checkAccessibility()
+	})
+	it('on the course list page',function(){
+		cy.visit('/user_courses')
+		cy.checkAccessibility()
+	})
+})
