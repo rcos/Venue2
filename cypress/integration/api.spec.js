6862c5f3f5435069a7879c1db4b54483eb56e1b4
TheStopsign <ethanwhitton@snowmobility.com>
2020-07-01 22:17:44 -0400

    playbackpoll api testing

--- cypress/integration/api.spec.js
+++ cypress/integration/api.spec.js
@@ -773,8 +773,10 @@ describe('API - Lecture Accessors',function() {
 })
 
 describe('API - Lecture Modifiers',function() {
-	beforeEach(() => {
+	before(() => {
 		cy.seed()
+	})
+	beforeEach(() => {
 		cy.setUser('testinst','password')
 	})
 	it('can addLecture()',function() {
@@ -798,29 +800,93 @@ describe('API - Lecture Modifiers',function() {
 })
 
 describe('API - PlaybackPoll Accessors',function() {
-	beforeEach(() => {
+	before(() => {
 		cy.seed()
+	})
+	beforeEach(() => {
 		cy.setUser('testinst','password')
+	})
+	let lectures = []
+	it('can getPolls()',function() {
+		cy.request({
+			method: 'GET',
+			url: 'http://localhost:4000/polls',
+			form: true,
+			headers: {
+				authorization: getJwt()
+			}
+		}).then(res => {
+			let polls = res.body
+			polls.forEach(poll => {
+				lectures.push(poll.lecture)
+			})
+			expect(polls.length).to.equal(5)
+		})
+	})
+	it('can getByLecture()',function() {
+		lectures.forEach(lecture => {
+			cy.request({
+				method: 'GET',
+				url: 'http://localhost:4000/polls/by_lecture/'+lecture,
+				form: true,
+				headers: {
+					authorization: getJwt()
+				}
+			}).then(res => {
+				let polls = res.body
+				expect(polls.length).to.be.at.least(1)
+			})
+		})
 	})
 })
 
 describe('API - PlaybackPoll Modifiers',function() {
-	beforeEach(() => {
+	before(() => {
 		cy.seed()
+	})
+	beforeEach(() => {
 		cy.setUser('testinst','password')
+	})
+	it('can addPoll()',function() {
+		cy.request({
+			method: 'POST',
+			url: 'http://localhost:4000/polls/add',
+			form: true,
+			headers: {
+				authorization: getJwt()
+			},
+			body: {
+				poll: {
+					question: "What are...?",
+					possible_answers: ["this","that","those"],
+					correct_answers: ["those"],
+					timestamp: 5
+				}
+			}
+		}).then(res => {
+			let addedPoll = res.body
+			expect(addedPoll.question).to.equal("What are...?")
+			expect(addedPoll.possible_answers).to.deep.equal(["this","that","those"])
+			expect(addedPoll.correct_answers).to.deep.equal(["those"])
+			expect(addedPoll.timestamp).to.equal(5)
+		})
 	})
 })
 
 describe('API - LectureSubmission Accessors',function() {
-	beforeEach(() => {
+	before(() => {
 		cy.seed()
+	})
+	beforeEach(() => {
 		cy.setUser('testinst','password')
 	})
 })
 
 describe('API - LectureSubmission Modifiers',function() {
-	beforeEach(() => {
+	before(() => {
 		cy.seed()
+	})
+	beforeEach(() => {
 		cy.setUser('testinst','password')
 	})
 })
