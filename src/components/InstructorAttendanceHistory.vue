8584e9b45148da7a023161caae15d69ab261b71d
TheStopsign <ejwhitton43@gmail.com>
2020-07-05 16:07:41 -0400

    no calls being made in courseinfo child components

--- src/components/InstructorAttendanceHistory.vue
+++ src/components/InstructorAttendanceHistory.vue
@@ -6,7 +6,7 @@
           <div class="month-area mobile">{{ STATIC_MONTHS[month] }} {{year}}</div>
           <div class="mobile-event-pill-area">
             <router-link v-for="i in timeline[year][month]" :key="lectures[i]._id" :to="{name: 'lecture_info', params: { lecture_id: lectures[i]._id }}" :aria-label="'Lecture Info - '+lectures[i].title">
-              <div :class="'mobile-pill ' + getClassByAttendance(lectures[i].percentage == undefined ? 0 : lectures[i].percentage)">
+              <div :class="'mobile-pill ' + getHTMLClassByAttendance(lectures[i].percentage == undefined ? 0 : lectures[i].percentage)">
                 <div class="day-of-week">{{ getDayOfWeek(lectures[i]) }}</div>
                 <div class="day-of-month">{{ getDayOfMonth(lectures[i]) }}</div>
               </div>
@@ -17,7 +17,7 @@
           <div class="month-area">{{ STATIC_MONTHS[month] }} {{year}}</div>
           <div class="event-pills-area">
             <router-link v-for="i in timeline[year][month]" :key="lectures[i]._id" :to="{name: 'lecture_info', params: { lecture_id: lectures[i]._id }}" tabindex="-1">
-              <div :class="'inline-block instructor-attendance-history-pill ' + getClassByAttendance(lectures[i].percentage == undefined ? 0 : lectures[i].percentage)" tabindex="0" :aria-label="'Lecture Info - '+lectures[i].title">
+              <div :class="'inline-block instructor-attendance-history-pill ' + getHTMLClassByAttendance(lectures[i].percentage == undefined ? 0 : lectures[i].percentage)" tabindex="0" :aria-label="'Lecture Info - '+lectures[i].title">
                 <div class="inline-block date-area">
                   <div class="day-of-week">{{ getDayOfWeek(lectures[i]) }}</div>
                   <div class="day-of-month">{{ getDayOfMonth(lectures[i]) }}</div>
@@ -27,7 +27,7 @@
                   <div class="event-location">Event Location</div>
                 </div>
                 <div class="inline-block percentage-area">
-                  <div v-if="scores_loaded">{{lectures[i].percentage == undefined ? 0 : lectures[i].percentage.toFixedDown(0)}}%</div>
+                  <div v-if="scores_loaded">{{lectures[i].percentage == undefined ? 0 : lectures[i].percentage.toFixedDecimals(0)}}%</div>
                   <SquareLoader v-else/>
                 </div>
               </div>
@@ -42,13 +42,6 @@
 <script>
 
   import SquareLoader from '@/components/Loaders/SquareLoader.vue'
-  import LectureSubmissionAPI from '@/services/LectureSubmissionAPI.js'
-
-  Number.prototype.toFixedDown = function(digits) {
-      var re = new RegExp("(\\d+\\.\\d{" + digits + "})(\\d)"),
-          m = this.toString().match(re);
-      return m ? parseFloat(m[1]) : this.valueOf();
-  };
 
   export default {
     name: 'InstructorAttendanceHistory',
@@ -57,17 +50,14 @@
       mobileMode: Boolean,
       lectures: Array,
       timeline: Object,
-      students: Array
+      students: Array,
+      scores_loaded: Boolean
     },
     data () {
       return {
         STATIC_MONTHS: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-        STATIC_DAY_OF_WEEK: ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat'],
-        scores_loaded: false
+        STATIC_DAY_OF_WEEK: ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat']
       }
-    },
-    mounted () {  
-      this.calculateAttendancePercentages()
     },
     methods: {
       shortenString (string, max_length) {
@@ -76,14 +66,11 @@
         }
         return string.substring(0, max_length - 3) + "..."
       },
-      getClassByAttendance (percent) {
+      getHTMLClassByAttendance (percent) {
         // percent b/w 0 and 100
         if (percent <= 60) return 'bad'
         else if (percent <= 75) return 'medium'
         return 'good'
-      },
-      getAttendancePercentage (event, section_id) {
-        return Math.floor((event.submission_count / this.sections_info[section_id].students.length) * 100)
       },
       getDayOfWeek (event) {
         let _date = new Date(event.start_time == undefined ? event.playback_submission_start_time : event.start_time)
@@ -92,61 +79,6 @@
       getDayOfMonth (event) {
         let _date = new Date(event.start_time == undefined ? event.playback_submission_start_time : event.start_time)
         return _date.getDate ()
-      },
-      async calculateAttendancePercentages () {
-        /*
-        * Calculating attendance percentage for instructor:
-        *
-        * For each attendance event, per section, find the number of students in this section
-        * and find the lecture submissions for a lecture. The percentage for that course is the
-        * number of submissions divided by the student body count then multiplied by 100.
-        */
-        let promise_tracker = []
-
-        this.lectures.forEach(lecture_ => {
-          // get the submission count for this lecture and then divide by the student count
-          
-          promise_tracker.push(
-            LectureSubmissionAPI.getLectureSubmissionsForLecture(lecture_._id)
-            .catch(err => { console.log('error retrieving lecture submissions for lecture ' + lecture_._id); console.log(err); })
-            .then(response => {
-              let submissions = response.data == undefined ? [] : response.data
-              let students = this.students
-              let running_total = 0
-              students.forEach(stud => {
-                let live = []
-                let playback = null
-                submissions.forEach(sub => {
-                  if(sub.submitter._id == stud._id) {
-                    if(sub.is_live_submission) {
-                      live.push(sub)
-                    } else {
-                      playback = sub
-                    }
-                  }
-                })
-                if(live.length > 0 && playback != null) {
-                  running_total += Math.max(
-                    live.length / lecture_.checkins.length,
-                    Math.ceil(playback.video_percent * 100) / 100
-                  )
-                } else if(live.length > 0) {
-                  running_total += live.length / lecture_.checkins.length
-                } else if(playback != null) {
-                  running_total += Math.ceil(playback.video_percent * 100) / 100
-                }
-              })
-              
-              lecture_.percentage = running_total / students.length * 100
-            })
-          )
-        })
-
-        Promise.all(promise_tracker)
-        .then(res => {
-          this.scores_loaded = true
-          this.$forceUpdate()
-        })
       },
       lecturesByDate(lectures) {
         return lectures.concat().sort((a, b) => (a.start_time < b.start_time) ? 1 : -1);
--- src/components/StudentAttendanceHistory.vue
+++ src/components/StudentAttendanceHistory.vue
@@ -6,7 +6,7 @@
           <div class="month-area mobile">{{ STATIC_MONTHS[month] }} {{year}}</div>
           <div class="mobile-event-pill-area">
             <router-link v-for="i in timeline[year][month]" :key="lectures[i]._id" :to="{name: 'lecture_info', params: { lecture_id: lectures[i]._id }}" :aria-label="'Lecture Info - '+lectures[i].title">
-              <div :class="'mobile-pill ' + getClassByAttendance(lectures[i].percentage == undefined ? 0 : lectures[i].percentage)">
+              <div :class="'mobile-pill ' + getHTMLClassByAttendance(lectures[i].percentage == undefined ? 0 : lectures[i].percentage)">
                 <div class="day-of-week">{{ getDayOfWeek(lectures[i]) }}</div>
                 <div class="day-of-month">{{ getDayOfMonth(lectures[i]) }}</div>
               </div>
@@ -17,7 +17,7 @@
           <div class="month-area">{{ STATIC_MONTHS[month] }} {{year}}</div>
           <div class="event-pills-area">
             <router-link v-for="i in timeline[year][month]" :key="lectures[i]._id" :to="{name: 'lecture_info', params: { lecture_id: lectures[i]._id }}" tabindex="-1">
-              <div :class="'inline-block instructor-attendance-history-pill ' + getClassByAttendance(lectures[i].percentage == undefined ? 0 : lectures[i].percentage)" tabindex="0" :aria-label="'Lecture Info - '+lectures[i].title">
+              <div :class="'inline-block instructor-attendance-history-pill ' + getHTMLClassByAttendance(lectures[i].percentage == undefined ? 0 : lectures[i].percentage)" tabindex="0" :aria-label="'Lecture Info - '+lectures[i].title">
                 <div class="inline-block date-area">
                   <div class="day-of-week">{{ getDayOfWeek(lectures[i]) }}</div>
                   <div class="day-of-month">{{ getDayOfMonth(lectures[i]) }}</div>
@@ -42,7 +42,6 @@
 <script>
 
   import SquareLoader from '@/components/Loaders/SquareLoader.vue'
-  import LectureSubmissionAPI from '@/services/LectureSubmissionAPI.js'
 
   export default {
     name: 'StudentAttendanceHistory',
@@ -50,19 +49,19 @@
     props: {
       mobileMode: Boolean,
       lectures: Array,
-      timeline: Object
+      timeline: Object,
+      scores_loaded: Boolean
     },
     data () {
       return {
         current_user: Object,
         STATIC_MONTHS: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-        STATIC_DAY_OF_WEEK: ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat'],
-        scores_loaded: false
+        STATIC_DAY_OF_WEEK: ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat']
       }
     },
     created () {
-      this.getCurrentUser ()
-      this.calculateGradeForHistoryData()
+      // this.getCurrentUser ()
+      // this.calculateGradeForHistoryData()
     },
     methods: {
       shortenString (string, max_length) {
@@ -74,66 +73,11 @@
       getCurrentUser() {
         this.current_user = this.$store.state.user.current_user
       },
-      getClassByAttendance (percent) {
+      getHTMLClassByAttendance (percent) {
         // percent b/w 0 and 100
         if (percent <= 60) return 'bad'
         else if (percent <= 75) return 'medium'
         return 'good'
-      },
-      calculateGradeForHistoryData () {
-        /* for each of the lectures in this section, we determine the grade by:
-         * (1) if the student submitted during the live lecture, they get a 100%
-         * (2) if the student did not attend the live lecture, and has not submitted to the
-         *      playback, regardless of whether the playback is available or not, they get a 0%
-         * (3) if the student does not attend the live lecture, but attends the playback lecture,
-         *      they grade is the percentage of the polls they got correct.
-        */
-
-        // First, get all lecture submissions from this student for each individual lecture
-        // If there is not a submission, return 0%
-        // If there is a submission, and is_live_submission == true, return 100%
-        // Otherwise, go through the student's poll answers and grade their poll answers
-        let promise_tracker = []
-
-        this.lectures.forEach(lecture_data => {
-
-          promise_tracker.push(
-            LectureSubmissionAPI.getLectureSubmissionsForStudent(lecture_data._id, this.current_user._id)
-            .catch(err => { console.log(`error retrieving lecture submissions for student ${this.current_user._id}`); console.log(err); })
-            .then(response => {
-              if (response.data == null || response.data == []) {
-                lecture_data.percentage = 0
-              } else {
-                let live = []
-                let playback = null
-                response.data.forEach(sub => {
-                  if(sub.submitter == this.current_user._id) {
-                    if(sub.is_live_submission) {
-                      live.push(sub)
-                    } else {
-                      playback = sub
-                    }
-                  }
-                })
-                if(live.length > 0 && playback != null) {
-                  lecture_data.percentage = Math.max(
-                    live.length / lecture_data.checkins.length * 100,
-                    Math.ceil(playback.video_percent * 100)
-                  )
-                } else if(live.length > 0) {
-                  lecture_data.percentage = live.length / lecture_data.checkins.length * 100
-                } else if(playback != null) {
-                  lecture_data.percentage = Math.ceil(playback.video_percent * 100)
-                }
-              }
-            })
-          )
-          Promise.all(promise_tracker)
-          .then(res => {
-            this.scores_loaded = true
-            this.$forceUpdate()
-          })
-        })
       },
       getDayOfWeek (event_info) {
         let date_ = new Date(event_info.start_time == undefined ? event_info.playback_submission_start_time : event_info.start_time)
