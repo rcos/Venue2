ead8d6740334d5c68c02d5ef67e459feac66ebda
TheStopsign <ejwhitton43@gmail.com>
2020-07-10 19:52:18 -0400

    sort courseinfo months and years

--- src/components/InstructorAttendanceHistory.vue
+++ src/components/InstructorAttendanceHistory.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="instructor-attendance">
-    <div v-for="year in Object.keys(timeline)" :key="year">
-      <div v-for="month in Object.keys(timeline[year])" :key="month">
+    <div v-for="year in getStringsDescending(Object.keys(timeline))" :key="year">
+      <div v-for="month in getStringsDescending(Object.keys(timeline[year]))" :key="month">
         <div v-if="mobileMode">
           <div class="month-area mobile">{{ STATIC_MONTHS[month] }} {{year}}</div>
           <div class="mobile-event-pill-area">
@@ -79,6 +79,9 @@
       getDayOfMonth (event) {
         let _date = new Date(event.start_time == undefined ? event.playback_submission_start_time : event.start_time)
         return _date.getDate ()
+      },
+      getStringsDescending(arr) {
+        return arr.concat().sort((a,b) => parseInt(a) < parseInt(b) ? 1 : -1)
       }
     }
   }
