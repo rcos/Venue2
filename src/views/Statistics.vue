7dcee551a5d823a882ac5a45e5084d846cf23b65
TheStopsign <ejwhitton43@gmail.com>
2020-07-11 22:10:05 -0400

    multiselect dropdown component

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -2,7 +2,22 @@
 	<div id="stats-container">
 		<div id="stats-left">
 			<div id="side-panel" class="stats-panel">
-
+				<div class="row side-panel-section">
+					<div class="col selector-label">
+						Course:
+					</div>
+					<div class="col col-8 selector-col">
+						<MultiSelectDropdown id="course-selector" v-if="course_options.length > 0" :options="course_options" @selection="handleCourseChange" :max="1"/>
+					</div>
+				</div>
+				<div class="row side-panel-section">
+					<div class="col selector-label">
+						Sections:
+					</div>
+					<div class="col col-8 selector-col">
+						<MultiSelectDropdown id="sections-selector" v-if="course_options.length > 0" :options="course_options" @selection="handleCourseChange" :max="4"/>
+					</div>
+				</div>
 			</div>
 		</div>
 		<div id="stats-right">
@@ -19,6 +34,10 @@
 </template>
 
 <script>
+import CourseAPI from '@/services/CourseAPI.js';
+
+import MultiSelectDropdown from '@/components/MultiSelectDropdown.vue';
+
 export default {
 	name: 'Statistics',
 	props: {
@@ -26,14 +45,36 @@ export default {
 	computed: {
 	},
 	components: {
+		MultiSelectDropdown
 	},
 	data(){
 		return {
+			current_user: null,
+			courses: [],
+			course_options: [],
+			selected_courses: [],
+			sections: [],
+			lectures: [],
+			students: []
 		}
 	},
 	created() {
+		this.current_user = this.$store.state.user.current_user;
+		this.fetchData()
 	},
 	methods: {
+		fetchData() {
+			CourseAPI.getInstructorCourses(this.current_user._id)
+			.then(res => {
+				this.courses = res.data
+				this.courses.forEach(course => {
+					this.course_options.push(course.name)
+				})
+			})
+		},
+		handleCourseChange(data) {
+
+		}
 	}
 }
 </script>
@@ -45,6 +86,15 @@ export default {
 	bottom: 0;
 	left: 0;
 	right: 0;
+}
+.row {
+	width: 100%;
+	margin: 0;
+	padding: 0;
+}
+.col {
+	margin: 0;
+	padding: 0;
 }
 
 #stats-left,
@@ -81,10 +131,32 @@ export default {
 	left: 1rem;
 	bottom: 1rem;
 	right: 1rem;
-	background: #868686;
+	background: #c4c4c4;
 	border-radius: 1rem;
 }
 #side-panel {
 	right: 0;
+}
+
+.side-panel-section {
+	padding: 1rem;
+}
+
+#course-section {
+	height: 2.5rem;
+	width: 18rem;
+	padding: 1rem;
+}
+#course-selector {
+	position: relative;
+	top: 0;
+}
+
+.selector-label {
+	text-align: right;
+}
+
+.selector-col {
+	width: 10rem;
 }
 </style>
