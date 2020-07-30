178747d3448fb0aafc2e8ae16f6239f702f2dff7
TheStopsign <ejwhitton43@gmail.com>
2020-07-30 00:20:58 -0400

    slight ui bug fix

--- server/LectureSubmission/LectureSubmission.model.js
+++ server/LectureSubmission/LectureSubmission.model.js
@@ -14,13 +14,13 @@ let LectureSubmission = new Schema({
 		ref: 'User'
 	},
 	codes: [String],
-	live_progress: Number,
-	live_percent: Number,
-	video_progress: Number,
-	video_percent: Number,
-	student_poll_answers: Object,
-	live_submission_time: Date,
-	playback_submission_time: Date
+	live_progress: Number, //			number of live checkins
+	live_percent: Number, // 			attendance percantage for live checkins
+	video_progress: Number, //			seconds into playback video
+	video_percent: Number, //			attendance percent for playback video
+	student_poll_answers: Object, //	mapping of <timestamp or percantage> as key, and <answer choices> and value
+	live_submission_time: Date, //		most recent live submission
+	playback_submission_time: Date //	most recent playback video submission
 });
 
 module.exports = mongoose.model('LectureSubmission', LectureSubmission);
