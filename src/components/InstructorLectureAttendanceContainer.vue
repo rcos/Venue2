8a50acf47e7a6734ddfb51d98e726053493a63bb
TheStopsign <ejwhitton43@gmail.com>
2020-08-11 16:07:06 -0400

    UI responsiveness

--- src/components/InstructorLectureAttendanceContainer.vue
+++ src/components/InstructorLectureAttendanceContainer.vue
@@ -12,7 +12,7 @@
 					<img src="@/assets/icons8-qr-code-50.png" width="60" alt="QR Code" aria-label="QR Code">
 				</button>
 			</div>
-			<button class="header-btn btn btn-primary" v-if="lectureIsOver() && !lecture.allow_playback_submissions" @click="handleEndEarly">End Now</button>
+			<button class="header-btn btn btn-primary" v-if="!lectureIsOver()" @click="handleEndEarly">End Now</button>
 			<LectureUploadModal v-if="lectureIsOver() && !lecture.allow_playback_submissions && polls_loaded" :lecture="lecture" :need_timestamp="polls" :update_lecture="true" />
 			<router-link class="header-btn btn btn-secondary" v-else-if="lecture.allow_playback_submissions" title="Watch Recording" :to="{name: 'lecture_playback', params: { lecture_id: lecture._id }}" aria-label="Watch Recording">
 				<img src="@/assets/icons8-video-64.png" width="60" alt="Video" aria-label="Video">
