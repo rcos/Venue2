81035b045c221a36f3cf7870d68bf1bf450ccf14
TheStopsign <ethanwhitton@snowmobility.com>
2020-08-11 19:30:46 -0400

    more

--- server/LectureSubmission/LectureSubmission.route.js
+++ server/LectureSubmission/LectureSubmission.route.js
@@ -257,12 +257,12 @@ lectureSubmissionRoutes.post('/update_all_to_new_model', (req, res) => {
 
           users.forEach(user => {
             lectures.forEach(lecture => {
-              submission_promises.push(new Promise((resolve,reject) => {
-                LectureSubmission.find(
-                {
-                  lecture: lecture._id,
-                  submitter: user._id
-                }, function(err,submissions){
+              LectureSubmission.find(
+              {
+                lecture: lecture._id,
+                submitter: user._id
+              }, function(err,submissions){
+                submission_promises.push(new Promise((resolve,reject) => {
                   console.log("SUBMISSIONS FOR USER",user.user_id,"AND LECTURE",lecture.title)
                   if(submissions && submissions.length) {
                     let new_submission_data = {
@@ -301,8 +301,8 @@ lectureSubmissionRoutes.post('/update_all_to_new_model', (req, res) => {
                       }
                     })
                   }
-                })
-              }))
+                }))
+              })
             })
           })
 
