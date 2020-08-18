dc27e635de861bc9610290e12b9b8c8eac587b41
TheStopsign <ejwhitton43@gmail.com>
2020-08-18 11:43:00 -0400

    change production url

--- src/services/API.js
+++ src/services/API.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 export default() => {
   return axios.create({
     baseURL: (process.env.NODE_ENV === 'production' ? 
-    	`https://venue-attend.herokuapp.com/` :
+    	`https://venue-meetings.herokuapp.com/` :
     	`http://localhost:4000/`),
     withCredentials: true
   })
