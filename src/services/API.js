a29a7ea80d694c9c7dc8cf6222d07b7d52fd801e
TheStopsign <ethanwhitton@snowmobility.com>
2020-08-18 21:05:57 -0400

    no more herokuapp website!

--- src/services/API.js
+++ src/services/API.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 export default() => {
   return axios.create({
     baseURL: (process.env.NODE_ENV === 'production' ? 
-    	`https://venue-meetings.herokuapp.com/` :
+    	`https://venue-meetings.com/` :
     	`http://localhost:4000/`),
     withCredentials: true
   })
