6cd22e90effd99a8a87ff5e11ec3aee26c7a4ddf
TheStopsign <ejwhitton43@gmail.com>
2020-08-19 13:39:14 -0400

    verify for string type in gcsSetup fields

--- gcsSetup.js
+++ gcsSetup.js
@@ -1,4 +1,4 @@
 if(process.env.NODE_ENV === 'production') {
 	const fs = require('fs');
-	fs.writeFile(process.env.GCP_KEY_FILE, process.env.GCP_CRED, (err) => {});
+	fs.writeFile(''+process.env.GCP_KEY_FILE, ''+process.env.GCP_CRED, (err) => {});
 }
