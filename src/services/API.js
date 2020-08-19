f27439559ff88761757379ba0ce5f654ec3a48ec
TheStopsign <ethanwhitton@snowmobility.com>
2020-08-19 09:51:26 -0400

    add www to add .com instances

--- src/services/API.js
+++ src/services/API.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 export default() => {
   return axios.create({
     baseURL: (process.env.NODE_ENV === 'production' ? 
-    	`https://venue-meetings.com/` :
+    	`https://www.venue-meetings.com/` :
     	`http://localhost:4000/`),
     withCredentials: true
   })
