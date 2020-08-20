d8f703f674aebfe579fe4e822015ccfde7953b1b
TheStopsign <ejwhitton43@gmail.com>
2020-08-20 18:22:19 -0400

    #app fills window

--- src/App.vue
+++ src/App.vue
@@ -102,10 +102,16 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
   // min-height: 100%;
 }
 
 main {
   padding-bottom: 2rem;
+  height: 100%;
 }
 </style>
