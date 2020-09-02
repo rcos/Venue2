8e6a60918311a036197115128242295d01c746b1
TheStopsign <ejwhitton43@gmail.com>
2020-09-02 17:11:30 -0400

    check the course

--- server/Lecture/Lecture.route.js
+++ server/Lecture/Lecture.route.js
@@ -357,7 +357,7 @@ lectureRoutes.get('/for_user/:user_id/:preference', (req, res) => {
 									// get the actual section object for the section reference
 									console.log("\tsection._id's:",student_sections.map(a=>a._id),typeof(student_sections[0]._id))
 									let lecture_section = student_sections.find(section => section._id.toString() == lecture_section_id.toString())
-									console.log('FOUND SECT:',lecture_section)
+									console.log('FOUND SECT:',lecture_section,lecture_section.course)
 									student_lecture.sections[i] = lecture_section
 									if(preference === "with_sections_and_course"){
 										course_promises.push(new Promise((resolve2,reject2) => {
