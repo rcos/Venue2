8afd7c760780dc4797562b26f83e14272b8662fb
TheStopsign <ejwhitton43@gmail.com>
2020-09-08 03:02:58 -0400

    quick navbar fix

--- src/components/UnrestrictedPlayback.vue
+++ src/components/UnrestrictedPlayback.vue
@@ -1,7 +1,8 @@
 <template>
 	<div id="unrestricted_playback">
 		<video id="video_player" class="video-js vjs-big-play-centered" controls>
-			<source v-bind:src="lecture.video_ref" :type="lecture.video_type">
+			<source v-if="isProduction" v-bind:src="lecture.video_ref + '?showinfo=0&enablejsapi=1&origin=https://www.venue-meetings.com'" :type="lecture.video_type">
+			<source v-else v-bind:src="lecture.video_ref + '?showinfo=0&enablejsapi=1&origin=http://localhost:8080'" :type="lecture.video_type">
 		</video>
 	</div>
 </template>
@@ -14,6 +15,9 @@ export default {
 	name: 'RestrictedPlayback',
 	props: {
 		lecture: Object
+	},
+	computed: {
+		isProduction(){ process.env.NODE_ENV === 'production' }
 	},
 	data() {
 		return {
