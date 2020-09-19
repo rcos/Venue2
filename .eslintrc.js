8e95f79b10591f4a3fcaae2bb99c4581e21a3d7c
TheStopsign <ejwhitton43@gmail.com>
2020-09-19 12:18:17 -0400

    actually add console logging

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,10 +4,10 @@ module.exports = {
     node: true
   },
   extends: ["plugin:vue/essential", "@vue/prettier"],
-  // rules: {
-  //   "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-  //   "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
-  // },
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+  },
   parserOptions: {
     parser: "babel-eslint"
   }
