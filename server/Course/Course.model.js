ae1e30b206000ee068ce84aa6c4a0df85f2b59e3
jen-rich <richaj10@rpi.edu>
2020-10-02 16:17:36 -0400

    Update Course.model.js
    added the ability for the course to hold multiple instructors

--- server/Course/Course.model.js
+++ server/Course/Course.model.js
@@ -3,15 +3,19 @@ const Schema = mongoose.Schema;
 
 let User = require('../User/User.model');
 
+//subschema to allow schema to hold multiple instructors
+let instructor = new Schema({
+	type: mongoose.Schema.Types.ObjectId,
+		ref: 'User'
+});
+
+
 //Define collection and schema for User
 let Course = new Schema({
 	name: String,
 	dept: String,
 	course_number: Number,
-	instructor: {
-		type: mongoose.Schema.Types.ObjectId,
-		ref: 'User'
-	}
+	instructors: [instructor]
 });
 
 module.exports = mongoose.model('Course', Course);
