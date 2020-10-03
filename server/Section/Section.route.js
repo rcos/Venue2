fb6214d8aa325a4b34f16a69473c3d52ceba2e3a
Joe Chu <chujoe26@gmail.com>
2020-10-02 20:13:22 -0400

    Standardized semicolon usage in course/section routes
    It was for my personal sanity

--- server/Section/Section.route.js
+++ server/Section/Section.route.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const sectionRoutes = express.Router();
-const jwt = require('jsonwebtoken')
-const ObjectID = require(`mongoose`).Types.ObjectId
+const jwt = require('jsonwebtoken');
+const ObjectID = require(`mongoose`).Types.ObjectId;
 
 let Section = require('./Section.model');
 let User = require('../User/User.model');
@@ -11,11 +11,11 @@ sectionRoutes.route('/add').post(function (req, res) {
   let section = new Section(req.body.section);
   section.save()
     .then(() => {
-      console.log("<SUCCESS> Adding section:",section)
+      console.log("<SUCCESS> Adding section:",section);
       res.status(200).json(section);
     })
     .catch(() => {
-      console.log("<ERROR> Adding section:",section)
+      console.log("<ERROR> Adding section:",section);
       res.status(400).send("unable to save section to database");
   });
 });
@@ -23,10 +23,10 @@ sectionRoutes.route('/add').post(function (req, res) {
 sectionRoutes.route('/').get(function (req, res) {
   Section.find(function(err, sections){
     if(err || sections == null) {
-      console.log("<ERROR> Getting all sections")
+      console.log("<ERROR> Getting all sections");
       res.json(err);
     } else {
-      console.log("<SUCCESS> Getting all sections")
+      console.log("<SUCCESS> Getting all sections");
       res.json(sections);
     }
   });
@@ -36,10 +36,10 @@ sectionRoutes.route('/edit/:id').get(function (req, res) {
   let id = req.params.id;
   Section.findById(id, function (err, section){
     if(err || section == null) {
-      console.log("<ERROR> Getting section with ID:",id)
+      console.log("<ERROR> Getting section with ID:",id);
       res.json(err);
     } else {
-      console.log("<SUCCESS> Getting section with ID:",id)
+      console.log("<SUCCESS> Getting section with ID:",id);
       res.json(section);
     }
   });
@@ -47,20 +47,20 @@ sectionRoutes.route('/edit/:id').get(function (req, res) {
 
 sectionRoutes.route('/update/:id').post(function (req, res) {
   let id = req.params.id;
-  let updated_section = {}
+  let updated_section = {};
   
   if(req.body.updated_section) {
     if(req.body.updated_section.course) {
-      updated_section.course = req.body.updated_section.course
+      updated_section.course = req.body.updated_section.course;
     }
     if(req.body.updated_section.name) {
-      updated_section.name = req.body.updated_section.name
+      updated_section.name = req.body.updated_section.name;
     }
     if(req.body.updated_section.students) {
-      updated_section.students = req.body.updated_section.students
+      updated_section.students = req.body.updated_section.students;
     }
     if(req.body.updated_section.teaching_assistants) {
-      updated_section.teaching_assistants = req.body.updated_section.teaching_assistants
+      updated_section.teaching_assistants = req.body.updated_section.teaching_assistants;
     }
   }
 
@@ -68,10 +68,10 @@ sectionRoutes.route('/update/:id').post(function (req, res) {
     updated_section,
     function(err, section) {
       if (err || section == null) {
-        console.log("<ERROR> Updating section by ID:",id,"with:",updated_section)
+        console.log("<ERROR> Updating section by ID:",id,"with:",updated_section);
         res.status(404).send("section not found");
       } else {
-        console.log("<SUCCESS> Updating section by ID:",id,"with:",updated_section)
+        console.log("<SUCCESS> Updating section by ID:",id,"with:",updated_section);
         res.json(section);
       }
     }
@@ -81,10 +81,10 @@ sectionRoutes.route('/update/:id').post(function (req, res) {
 sectionRoutes.route('/delete/:id').delete(function (req, res) {
   Section.findByIdAndRemove({_id: req.params.id}, function(err){
     if(err) {
-      console.log("<ERROR> Deleting section with ID:",req.params.id)
+      console.log("<ERROR> Deleting section with ID:",req.params.id);
       res.json(err);
     } else {
-      console.log("<SUCCESS> Deleting section with ID:",req.params.id)
+      console.log("<SUCCESS> Deleting section with ID:",req.params.id);
       res.json('Successfully removed');
     }
   });
@@ -94,23 +94,23 @@ sectionRoutes.route('/getInstructors/:id').get(function (req, res) {
   let id = req.params.id;
   Section.findById(id, function (err, section){
       if(err || section == null) {
-        console.log("<ERROR> Getting section with ID:",id)
+        console.log("<ERROR> Getting section with ID:",id);
         res.json(err);
       } else {
         let course_id = section.course;
         Course.findById(course_id, function(error, course){
           if(error || course == null){
-            console.log("<ERROR> Getting course with ID:",course_id)
+            console.log("<ERROR> Getting course with ID:",course_id);
             res.json(error);
           } else {
             let instructor_ids = course.instructors;
             User.find({_id: {$in: instructor_ids}}, function(error, instructors){
               if(error || instructors == null) {
-                console.log("<ERROR> Getting instructors with IDs:",instructor_id)
-                res.json(error)
+                console.log("<ERROR> Getting instructors with IDs:",instructor_id);
+                res.json(error);
               } else {
-                console.log("<SUCCESS> Getting instructors for section with ID:",id)
-                res.json(instructors)
+                console.log("<SUCCESS> Getting instructors for section with ID:",id);
+                res.json(instructors);
               }
             });
           }
@@ -123,16 +123,16 @@ sectionRoutes.route('/getCourse/:id').get(function (req, res) {
   let id = req.params.id;
   Section.findById(id, function (err, section){
     if(err || section == null) {
-      console.log("<ERROR> Getting section with ID:",id)
+      console.log("<ERROR> Getting section with ID:",id);
       res.json(err);
     } else {
       let course_id = section.course;
       Course.findById(course_id, function(error, course){
         if(error || course == null) {
-          console.log("<ERROR> Getting course with ID:",course_id)
+          console.log("<ERROR> Getting course with ID:",course_id);
           res.json(error);
         } else {
-          console.log("<SUCCESS> Getting course for section with ID:",id)
+          console.log("<SUCCESS> Getting course for section with ID:",id);
           res.json(course);
         }
       });
@@ -144,7 +144,7 @@ sectionRoutes.route('/getStudents/:id').get(function (req, res) {
   let id = req.params.id;
   Section.findById(id, function (err, section){
     if(err || section == null) {
-      console.log("<ERROR> Getting section with ID:",id)
+      console.log("<ERROR> Getting section with ID:",id);
       res.json(err);
     } else {
       let student_ids = section.students;
@@ -154,30 +154,30 @@ sectionRoutes.route('/getStudents/:id').get(function (req, res) {
         student_ids.forEach(student_id => {
           User.findById(student_id, function(err, student) {
             if(err || student == null){
-              console.log("<ERROR> Getting user with ID:",student_id)
+              console.log("<ERROR> Getting user with ID:",student_id);
               res.json(err);
             } else {
               students.push(student);
               num_iterations++;
               if(num_iterations === student_ids.length) {
-                console.log("<SUCCESS> Getting students for section with ID:",id)
+                console.log("<SUCCESS> Getting students for section with ID:",id);
                 res.json(students);
               }
             }
-          })
-        })
+          });
+        });
       } else {
-        res.json([])
+        res.json([]);
       }
     }
-  })
-})
+  });
+});
 
 sectionRoutes.route('/getTeachingAssistants/:id').get(function (req, res) {
   let id = req.params.id;
   Section.findById(id, function (err, section){
     if(err || section == null) {
-      console.log("<ERROR> Getting section with ID:",id)
+      console.log("<ERROR> Getting section with ID:",id);
       res.json(err);
     } else {
       let ta_ids = section.teaching_assistants;
@@ -187,174 +187,174 @@ sectionRoutes.route('/getTeachingAssistants/:id').get(function (req, res) {
         ta_ids.forEach(ta_id => {
           User.findById(ta_id, function(err, ta) {
             if(err || ta == null){
-              console.log("<ERROR> Getting user with ID:",ta_id)
+              console.log("<ERROR> Getting user with ID:",ta_id);
               res.json(err);
             } else {
               tas.push(ta);
               num_iterations++;
               if(num_iterations === ta_ids.length) {
-                console.log("<SUCCESS> Getting teaching assistants for section with ID:",id)
+                console.log("<SUCCESS> Getting teaching assistants for section with ID:",id);
                 res.json(tas);
               }
             }
-          })
-        })
+          });
+        });
       } else {
-        res.json([])
+        res.json([]);
       }
     }
-  })
-})
+  });
+});
 
-sectionRoutes.get('/get_with_courses_for_student/:user_id', (req, res) => {
-  let user_id = req.params.user_id
-  user_sections = []
+sectionRoutes.get('/get_with_courses_for_student/:user_id', (req, res) => {//todo here
+  let user_id = req.params.user_id;
+  user_sections = [];
   Section.find((error, sections) => {
     if(error || sections == null) {
-      console.log("<ERROR> Getting all sections")
-      res.json(error)
+      console.log("<ERROR> Getting all sections");
+      res.json(error);
     } else {
       sections.forEach((section) => {
         section.students.forEach((student) => {
           if(student._id == user_id)
-            user_sections.push(section)
-        })
-      })
-      let counter = 0
+            user_sections.push(section);
+        });
+      });
+      let counter = 0;
       user_sections.forEach((user_section) => {
         Course.findById(user_section.course, function (course_error, course){
           if(course_error || course == null) {
-            console.log("<ERROR> Getting course with ID:",user_section.course)
+            console.log("<ERROR> Getting course with ID:",user_section.course);
             res.json(course_error);
           } else { 
-            user_section.course = course
+            user_section.course = course;
           }
-          counter++
+          counter++;
           if(counter === user_sections.length){
-            console.log("<SUCCESS> Getting sections with courses for user with ID:",user_id)
-            res.json(user_sections)
+            console.log("<SUCCESS> Getting sections with courses for user with ID:",user_id);
+            res.json(user_sections);
           }
-        })
-      })
+        });
+      });
     }
-  })
-})
+  });
+});
 
 sectionRoutes.get('/get_with_course/:section_id', (req, res) => {
-  let section_id = req.params.section_id
+  let section_id = req.params.section_id;
   Section.findById(section_id, function (err, section){
     if(err || section == null) {
-      console.log("<ERROR> Getting section with ID:",section_id)
+      console.log("<ERROR> Getting section with ID:",section_id);
       res.json(err);
     } else {
       Course.findById(section.course, function(error, course) {
         if(error || course == null) {
-          console.log("<ERROR> Getting course with ID:",section.course)
-          res.json(error)
+          console.log("<ERROR> Getting course with ID:",section.course);
+          res.json(error);
         } else {
-          section.course = course
-          console.log("<SUCCESS> Getting section with course for section with ID:",section_id)
-          res.json(section)
+          section.course = course;
+          console.log("<SUCCESS> Getting section with course for section with ID:",section_id);
+          res.json(section);
         }
-      })
+      });
     }
-  })
-})
+  });
+});
 
 sectionRoutes.get('/get_for_course/:course_id', (req, res) => {
-  let course_id = req.params.course_id
+  let course_id = req.params.course_id;
   Section.find((error, sections) => {
     if(error || sections == null) {
-      console.log("<ERROR> Getting all sections")
-      res.json(error)
+      console.log("<ERROR> Getting all sections");
+      res.json(error);
     } else {
-      let course_sections = []
+      let course_sections = [];
       sections.forEach(section => {
         if(section.course == course_id)
-          course_sections.push(section)
-      })
-      console.log("<SUCCESS> Getting all sections for course with ID:",course_id)
-      res.json(course_sections)
+          course_sections.push(section);
+      });
+      console.log("<SUCCESS> Getting all sections for course with ID:",course_id);
+      res.json(course_sections);
     }
-  })
-})
+  });
+});
 
 sectionRoutes.get('/get_ta_sections', (req, res) => {
   Section.find({_id: {$in: req.user.ta_sections}}, function(err, ta_sections) {
     if(err || ta_sections == null) {
-      console.log("<ERROR> Getting ta sections by user with ID:",req.user._id)
-      res.json(err)
+      console.log("<ERROR> Getting ta sections by user with ID:",req.user._id);
+      res.json(err);
     } else {
-      let promises = []
+      let promises = [];
       ta_sections.forEach(section => {
         promises.push(new Promise((resolve,reject) => {
           Course.findById(section.course,function(err,course) {
             section.course = course
             resolve(course)
-          })
-        }))
-      })
+          });
+        }));
+      });
       Promise.all(promises).then(resolved => {
-        console.log("<SUCCESS> Getting ta sections by user with ID:",req.user._id)
-        res.json(ta_sections)
-      })
-    }
-  })
-})
+        console.log("<SUCCESS> Getting ta sections by user with ID:",req.user._id);
+        res.json(ta_sections);
+      });
+    };
+  });
+});
 
 sectionRoutes.get('/get_student_sections', (req, res) => {
   Section.find({_id: {$in: req.user.student_sections}}, function(err, student_sections) {
     if(err || student_sections == null) {
-      console.log("<ERROR> Getting ta sections by user with ID:",req.user._id)
-      res.json(err)
+      console.log("<ERROR> Getting ta sections by user with ID:",req.user._id);
+      res.json(err);
     } else {
-      let promises = []
+      let promises = [];
       student_sections.forEach(section => {
         promises.push(new Promise((resolve,reject) => {
           Course.findById(section.course,function(err,course) {
-            section.course = course
-            resolve(course)
+            section.course = course;
+            resolve(course);
           })
         }))
       })
       Promise.all(promises).then(resolved => {
         console.log("<SUCCESS> Getting student sections by user with ID:",req.user._id)
-        res.json(student_sections)
-      })
+        res.json(student_sections);
+      });
     }
-  })
-})
+  });
+});
 
 sectionRoutes.post('/add_students/:id', (req, res) => {
-  let student_emails = req.body.students
-  let section_id = req.params.id
+  let student_emails = req.body.students;
+  let section_id = req.params.id;
   Section.findById(section_id,function(err,section) {
     User.find({email: {$in: student_emails}},function(err,students) {
-      let student_ids = students.map(a => a._id)
+      let student_ids = students.map(a => a._id);
       Promise.all([
         User.updateMany( {_id: {$in: student_ids}}, {$push: {student_sections: [section_id]}}),
         Section.findByIdAndUpdate( section_id, {$push: {students: {$each: student_emails}}})
       ]).then(resolved => {
-        res.json()
-      })
-    })
-  })
-})
+        res.json();
+      });
+    });
+  });
+});
 
 sectionRoutes.post('/add_tas/:id', (req, res) => {
-  let ta_emails = req.body.tas
-  let section_id = req.params.id
+  let ta_emails = req.body.tas;
+  let section_id = req.params.id;
   Section.findById(section_id,function(err,section) {
     User.find({email: {$in: ta_emails}},function(err,tas) {
-      let ta_ids = tas.map(a => a._id)
+      let ta_ids = tas.map(a => a._id);
       Promise.all([
         User.updateMany( {_id: {$in: ta_ids}}, {$push: {ta_sections: [section_id]}}),
         Section.findByIdAndUpdate( section_id, {$push: {tas: {$each: ta_emails}}})
       ]).then(resolved => {
-        res.json()
-      })
-    })
-  })
-})
+        res.json();
+      });
+    });
+  });
+});
 
 module.exports = sectionRoutes;
