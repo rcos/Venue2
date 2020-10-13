29ae5b9ef877474409e25a85167122d6f7e0abed
jen-rich <richaj10@rpi.edu>
2020-10-13 17:20:25 -0400

    Update Section.model.js
    Allows a specific course section to be public

--- server/Section/Section.model.js
+++ server/Section/Section.model.js
@@ -17,7 +17,11 @@ let Section = new Schema({
 	students: [{
 		type: mongoose.Schema.Types.ObjectId,
 		ref: 'User'
-	}]
+	}],
+	is_public: {
+		type: Boolean,
+		default: 'False'
+	}
 });
 
 module.exports = mongoose.model('Section', Section);
