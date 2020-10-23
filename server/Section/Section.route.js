bf4c1ff8bd5402f48ea6b76614af444e25f88cba
jen-rich <richaj10@rpi.edu>
2020-10-23 17:41:22 -0400

    Name changes of variables
    Just changed names of variables in section.route and course.route

--- server/Section/Section.route.js
+++ server/Section/Section.route.js
@@ -337,7 +337,7 @@ sectionRoutes.post('/add_tas/:id', (req, res) => {
       let ta_ids = tas.map(a => a._id)
       Promise.all([
         User.updateMany( {_id: {$in: ta_ids}}, {$push: {ta_sections: [section_id]}}),
-        Section.findByIdAndUpdate( section_id, {$push: {teaching_assistants: {$each: ta_ids}}})
+        Section.findByIdAndUpdate( section_id, {$push: {tas: {$each: ta_ids}}})
       ]).then(resolved => {
         res.json()
       })
