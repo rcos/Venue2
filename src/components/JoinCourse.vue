c719437d8425ed064213a1e8fbef85f0e4d7cac8
Cheng Bin Chen <chengbinchen1@gmail.com>
2020-10-25 02:18:28 -0400

    10.25

--- src/components/JoinCourse.vue
+++ src/components/JoinCourse.vue
@@ -14,11 +14,11 @@ export default {
     this.course_id = this.$route.params.id
     this.current_user = this.$store.state.user.current_user
     this.inCourse()
-   
-  }, 
+    this.addToSection()
+  },
+
   methods : {
-    async inCourse()  {
-      let in_course = false
+    inCourse()  {
       SectionAPI.getSectionsForCourse(this.course_id).then(res=> {
         res.data.forEach(sec => {
           this.sections.push(sec._id)
@@ -26,24 +26,24 @@ export default {
         })
         for (let i = 0; i < this.sections.length; i++) {
           if (this.current_user.student_sections.includes(this.sections[i])) {
-           in_course = !in_course
-            this.$router.push('/course_info/' + this.sections[i])
+            this.$router.push({name: 'course_info', params: {id: this.sections[i]}})
             break
           }
         }
-        if (!in_course) {
-          var section_choice = prompt("Join Section")
-          for (let i = 0; i < this.names.length; i++) {
-              if (this.names[i] == section_choice) {
-                const response = SectionAPI.addToSection(this.sections[i], this.current_user._id)
-                this.$router.push('/course_info/' + this.sections[i])
-                location.reload()
-              }
-          }
-        }
       })
-    }
+    }, 
 
+    async addToSection() {
+      var section_choice = prompt("Join Section")
+      for (let i = 0; i < this.names.length; i++) {
+        console.log(this.names[i])
+        if (this.names[i] == section_choice) {
+          const response = SectionAPI.addToSection(this.sections[i], this.current_user._id)
+          this.$router.push({name: 'course_info', params: {id: this.sections[i]}})
+          location.reload()
+        }
+      }
+    }
 
   }
 }
