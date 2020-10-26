6d24ce571dcc10e4825331c9197b247d34254938
thestopsign <ethanwhitton@snowmobility.com>
2020-10-26 13:29:48 -0400

    fix css loading

--- src/App.vue
+++ src/App.vue
@@ -112,7 +112,7 @@ export default {
 
 <style lang="scss">
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: "Comfortaa", "Exo", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
