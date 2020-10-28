0bba5fe6a1d48f52d728da2b33a31484bfcfc6ce
baumgartner-brody <43212689+baumgartner-brody@users.noreply.github.com>
2020-10-28 13:56:59 -0400

    Add files via upload

--- src/views/CourseInfo.vue
+++ src/views/CourseInfo.vue
@@ -5,12 +5,15 @@
 
         <!-- Title -->
         <div class="title">
-          <router-link v-if="is_instructor || is_ta" :to="{name: 'new_lecture', params: { course_id: (is_instructor?course._id:$route.params.id) }}" tabindex="-1">
+          <router-link v-if="is_instructor || is_ta" :to="{name: 'new_lecture', params: { course_id: $route.params.id }}" tabindex="-1">
             <div class="inline-block big-button" :style="{float: 'right'}" tabindex="0">Create New Lecture for {{ course.prefix }} {{ course.suffix }}</div>
           </router-link>
         </div>
-        <CourseInfoTitle :course="typeof course == typeof {} ? course : {}" class="inline-block" :section_name="section.name" :is_instructor="is_instructor" :is_ta="is_ta"/>
 
+        <button v-if="is_instructor || is_ta" class="inline-block big-button" :style="{float: 'right'}" tabindex="0" @click="copyURL">Copy Link to Join {{ course.prefix }} {{ course.suffix }}</button>        
+
+        <CourseInfoTitle v-if="is_instructor" :course="course" :section_name="sorted_sections.map(a => a.name).join(', ')" class="inline-block" :is_instructor="is_instructor" :is_ta="is_ta"/>
+        <CourseInfoTitle v-else-if="sections[$route.params.id]" :course="sections[$route.params.id].course" :section_name="sections[$route.params.id].name" class="inline-block" :is_instructor="is_instructor" :is_ta="is_ta"/>
 
         <!-- Attendance History -->
         <div>
@@ -41,10 +44,13 @@
     <hide-at breakpoint="large">
       <div>
         <!-- Mobile View -->
-        <CourseInfoTitle :course="typeof course == typeof {} ? course : {}" class="inline-block" :is_instructor="is_instructor" :is_ta="is_ta" mobileMode />
-        <router-link v-if="is_instructor || is_ta" :to="{name: 'new_lecture', params: { course_id: (is_instructor?course._id:$route.params.id) }}" tabindex="-1">
+        <CourseInfoTitle v-if="is_instructor" :course="course" :section_name="sorted_sections.map(a => a.name).join(', ')" class="inline-block" :is_instructor="is_instructor" :is_ta="is_ta" mobileMode/>
+        <CourseInfoTitle v-else-if="sections[$route.params.id]" :course="sections[$route.params.id].course" :section_name="sections[$route.params.id].name" class="inline-block" :is_instructor="is_instructor" :is_ta="is_ta" mobileMode/>
+
+        <router-link v-if="is_instructor || is_ta" :to="{name: 'new_lecture', params: { course_id: $route.params.id }}" tabindex="-1">
           <div class="inline-block big-button mobile" tabindex="0">Create New Lecture for {{ course.prefix }} {{ course.suffix }}</div>
         </router-link>
+        <button v-if="is_instructor || is_ta" class="inline-block big-button mobile" :style="{float: 'right'}" tabindex="0" @click="copyURL">Copy Link to Join {{ course.prefix }} {{ course.suffix }}</button>
         <div class="courseinfo-attendance-listing">
           <div v-if="is_instructor" class="section-select-container mobile float-right">
             <label id="section_select_label">Section(s):</label>
@@ -57,9 +63,9 @@
           <div class="courseinfo-legend live-border">Synchronous</div>
           <div class="courseinfo-legend playback-border">Asynchronous</div>
           <InstructorAttendanceHistory :style='{textAlign: "center"}'
-            v-if="(is_instructor || is_ta) && lectures_loaded && sorted_lectures[selected_section] && scores_loaded"
+            v-if="(is_instructor || is_ta) && lectures_loaded && sorted_lectures[selected_section]"
             :lectures="sorted_lectures[selected_section].lectures" :timeline="sorted_lectures[selected_section].timeline" :students="selected_section == 'all' ? course_students : sections[selected_section].students" :scores_loaded="scores_loaded" mobileMode/>
-          <StudentAttendanceHistory :style='{textAlign: "center"}' v-else-if="is_student && lectures_loaded && sorted_lectures[section_id] && scores_loaded" :lectures="sorted_lectures[section_id].lectures" :timeline="sorted_lectures[section_id].timeline" :scores_loaded="scores_loaded" mobileMode/>
+          <StudentAttendanceHistory :style='{textAlign: "center"}' v-else-if="is_student && lectures_loaded && sorted_lectures[section_id]" :lectures="sorted_lectures[section_id].lectures" :timeline="sorted_lectures[section_id].timeline" :scores_loaded="scores_loaded" mobileMode/>
           <div v-else-if="!lectures_loaded" :style='{textAlign: "center"}'>
             <SquareLoader />
           </div>
@@ -144,12 +150,16 @@ export default {
       this.getAllSections()
       this.getStudentsForCourse()
       this.getCourse()
-    } else {
+    }
+    else {
       this.section_id = this.$route.params.id
       this.getSectionWithCourse()
     }
   },
   methods: {
+    copyURL() {
+      navigator.clipboard.writeText((process.env.NODE_ENV === 'production'?'https://www.venue-meetings.com':'http://localhost:8080')+"/#/join_course/"+this.course._id)
+    },
     async getAllSections () {
       SectionAPI.getSectionsForCourse(this.course_id)
       .catch(err => { console.log(`Problem getting sections for course ${this.course_id}`); console.log(err);})
@@ -337,18 +347,18 @@ export default {
         return "rgb(" + [color.r, color.g, color.b].join(",") + ")";
     },
     calculateInstructorAttendances() {
-      let promise_tracker = []
-      this.all_lectures.forEach(lecture_ => {
-        promise_tracker.push(
-          LectureSubmissionAPI.getLectureSubmissionsForLecture(lecture_._id)
-          .catch(err => { console.log('error retrieving lecture submissions for lecture ' + lecture_._id); console.log(err); })
-          .then(response => {
-            let submissions = response.data == undefined ? [] : response.data
+      LectureSubmissionAPI.getLectureSubmissionsForLectures(this.all_lectures.map(a=>a._id))
+      .catch(err => { console.log('error retrieving lecture submissions for lecture ' + lecture_._id); console.log(err); })
+      .then(response => {
+        let promise_tracker = []
+        this.all_lectures.forEach(lecture_ => {
+          promise_tracker.push( new Promise((resolve,reject) => {
+            let submissions = response.data == undefined ? [] : response.data.filter(a=>a.lecture == lecture_._id)
             let students = lecture_.students
             let running_total = 0
             students.forEach(stud => {
               submissions.forEach(submission => {
-                if(submission.submitter._id == stud) {
+                if(submission.submitter.email == stud) {
                   if(submission.live_percent) {
                     if(submission.video_percent) {
                       running_total += Math.max(
@@ -367,12 +377,13 @@ export default {
             lecture_.percentage = running_total / students.length
             // lecture_.color = this.getHTMLClassByAttendance(lecture_.percentage)
             lecture_.color = this.perc2color(lecture_.percentage / 100)
-          })
-        )
-      })
-      Promise.all(promise_tracker)
-      .then(res => {
-        this.scores_loaded = true
+            resolve(true)
+          }))
+        })
+        Promise.all(promise_tracker)
+        .then(res => {
+          this.scores_loaded = true
+        })
       })
     },
     calculateStudentAttendances() {
