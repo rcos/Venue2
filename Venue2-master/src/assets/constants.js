71c9b76466f352214c5d652f7f9801655bc1c4d1
baumgartner-brody <bradluigi@gmail.com>
2020-10-31 16:14:58 -0400

    updated with master

--- Venue2-master/src/assets/constants.js
+++
@@ -1,5 +0,0 @@
-let Constants = {
-  colors: ['Aquamarine', 'Tomato', 'LightSalmon', 'Cyan', 'MediumTurquoise', 'PaleGreen', 'pink', 'violet', 'aqua', 'BurlyWood', 'Chartreuse', 'Crimson', 'CadetBlue', 'Chocolate', 'BlueViolet', 'DarkRed', 'Lime', 'PowderBlue']
-}
-
-export default Constants
