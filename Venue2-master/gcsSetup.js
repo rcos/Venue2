26592322734858b117384cd93ad6768626e13074
baumgartner-brody <bradluigi@gmail.com>
2020-10-31 16:14:58 -0400

    updated with master

--- Venue2-master/gcsSetup.js
+++
@@ -1,4 +0,0 @@
-if(process.env.NODE_ENV === 'production') {
-	const fs = require('fs');
-	fs.writeFile(process.env.GCS_KEY_FILE, process.env.GCS_CRED, (err) => {});
-}
--- Venue2-master/src/services/PlatformAPI.js
+++
@@ -1,6 +0,0 @@
-import {isMobile, isIE} from 'react-device-detect'
-
-export default PlatformAPI = {
-  isMobile: () => isMobile,
-  isIE: () => isIE
-}
