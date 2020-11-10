531eed3aab720a238d5adf66b67b2e1428156f1b
Aasiya <husais3@rpi.edu>
2020-11-10 12:52:48 -0500

    added 4 icons

--- src/components/CourseInfoTitle.vue
+++ src/components/CourseInfoTitle.vue
@@ -2,7 +2,7 @@
   <div :class="'course-info-title ' + (this.mobileMode ? 'mobile' : '')">
 
     <!-- Course Name -->
-    <div class="course-name" v-if="course.hasOwnProperty('name')"><div v-if="mobileMode && (is_instructor || is_ta)" class="icon-spacer"/> {{course.name}} <button class="btn" title="Edit Course" id="edit-course" @click="handleEditCourse()" v-if="is_instructor || is_ta"><img class="svg-color" id="edit-course" src="@/assets/settings.svg" alt="Edit" width="40" aria-label="Edit"></button></div>
+    <div class="course-name" v-if="course.hasOwnProperty('name')"><div v-if="mobileMode && (is_instructor || is_ta)" class="icon-spacer"/> {{course.name}} <button class="btn" title="Edit Course" id="edit-course" @click="handleEditCourse()" v-if="is_instructor || is_ta"><img class="svg-color" id="edit-course" src="@/assets/venue-settings.svg" alt="Edit" width="40" aria-label="Edit"></button></div>
 
     <div>
 
